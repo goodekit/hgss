15b044e899085dd96843ce5d9d4ff0c5aa37c728
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-06 22:46:58 +1300

    add shared component exports for bag, drawer, header, footer, and protection features

---
+++ hgss/component/shared/index.ts
@@ -0,0 +1,17 @@
+// ⎿ bag
+export { default as AddToBag } from './bag/add-to-bag'
+export { default as BagIconWithBadge } from './bag/bag-icon-with-badge'
+// ⎿ drwr
+export { default as CategoryDrwr } from './drwr/category-drwr'
+// ⎿header
+export { default as Header } from './header/header'
+export { default as MobileMenu } from './header/mobile-menu'
+export { default as Search } from './header/search'
+export { default as ThemeToggle } from './header/theme-toggle'
+export { default as Toolbar } from './header/toolbar'
+export { default as UserMenu } from './header/user-menu'
+// ⎿ footer
+export { default as Footer } from './footer/footer'
+// ⎿ protect
+export { default as ProtectNavLink } from './protect/protect-nav-link'
+export { default as ProtectNavDialg } from './protect/protect-nav-dialg'
