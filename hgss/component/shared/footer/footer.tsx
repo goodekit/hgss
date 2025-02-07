c354eee56e4b27970b6cd034f5728642f0c0a55b
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-07 18:24:04 +1300

    [refax]: footer component to remove unnecessary border styling

--- hgss/component/shared/footer/footer.tsx
+++ hgss/component/shared/footer/footer.tsx
@@ -3,7 +3,7 @@ import { GLOBAL } from 'hgss'
 const Footer = () => {
   const currentYear = new Date().getFullYear()
   return (
-    <footer className="border-t">
+    <footer>
       <div className="p-5 flex-center">
         {currentYear} &copy; {GLOBAL.APP_NAME}. All rights reserved
       </div>
