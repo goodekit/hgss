09295fa1a8d6194eff03a9bbcc24485720d1cd1a
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-07 18:19:28 +1300

    [db]:  add mock data for users and products

---
+++ hgss/_mock/_mock-data.ts
@@ -0,0 +1,207 @@
+export const _mockData = {
+    users: [
+        {
+          name    : 'Admin User',
+          email   : 'admin@vieux-carre.com',
+          role    : 'admin',
+          password: '123456'
+        },
+        {
+          name    : 'Hank Hill',
+          email   : 'propane.love@strickland.com',
+          role    : 'user',
+          password: '123456'
+        },
+        {
+          name    : 'Dale Gribble',
+          email   : 'dale@dale-tech.com',
+          role    : 'user',
+          password: '123456'
+        },
+        {
+          name    : 'Bill Dauterive',
+          email   : 'bill.d@usarmy.com',
+          role    : 'user',
+          password: '123456'
+        },
+        {
+          name    : 'Jeff Boomhauer',
+          email   : 'boomhauer@yahoo.com',
+          role    : 'user',
+          password: '123456'
+        },
+        {
+          name    : 'Rusty Shackleford',
+          email   : 'rusty@godaddy.com',
+          role    : 'user',
+          password: '123456'
+        }
+      ],
+    products: [
+        {
+          id            : 1,
+          model         : '[EP70/71] Toyota Starlet',
+          name          : 'Starlet Cupholder',
+          description   : 'Our tough ABS plastic cupholder utilises the spring and screws from your OEM ashtray and instals in the same manor.  Made to securely hold 2x standard 355ml cans.  500ml cans with the same base diameter will fit but touch against the dashboard.',
+          title         : 'Replace your ashtray with something more useful!',
+          price         : 19.99,
+          images        : ['/_mock/1/prod1.png', '/_mock/1/prod2.png', '/_mock/1/prod3.png', '/_mock/1/prod4.png', '/_mock/1/prod5.png'],
+          specifications: ['Made from premium materials', 'Lasts a lifetime', 'Eco-friendly', 'Comes in multiple colors'],
+          brand         : 'Toyota',
+          stock         : 10,
+          category      : 'Automotive',
+          reviews       : [],
+          rating        : 0,
+          numReviews    : 0
+        },
+        {
+          id            : 2,
+          model         : '[EP70/71] Toyota Starlet',
+          name          : 'Guage Pod',
+          title         : 'No drilling holes required!',
+          description   : 'This gauge pod is specifically designed for EP70/71 Starlets and replaces the OEM clock and bezel.  With it’s tough ABS plastic construction it looks at home atop the dashboard.  Designed for standard 52mm gauges.  Please note that outer gauges may only be able to fasten inner nut to retain, depending on length of gauge.',
+          price         : 29.99,
+          images        : ['/_mock/2/prod1.png', '/_mock/2/prod2.png', '/_mock/2/prod3.png', '/_mock/2/prod4.png', '/_mock/2/prod5.png'],
+          specifications: ['Made from premium materials', 'Lasts a lifetime', 'Eco-friendly', 'Comes in multiple colors'],
+          brand         : 'Toyota',
+          stock         : 10,
+          category      : 'Automotive',
+          reviews       : [],
+          rating        : 0,
+          numReviews    : 0
+        },
+        {
+          id         : 3,
+          model      : '2EE Toyota',
+          name       : 'ITB Kit',
+          title      : 'Quad throttle supreme noise!',
+          description: `
+              This Individual throttle body kit is designed as a bolt on unit for the Toyota 2EE Engine.  It sports all the necessary components to get your 2EE converted to ITBS!
+                  -40mm Throttle bodies
+                  -Carbon Fibre Nylon runners
+                  -Aluminium cylinder head flange
+                  -Your choice of Carbon fibre Nylon or Chrome plated velocity stacks
+                  -Linkages and throttle arms allow for fine adjustment of butterflies
+                  -Vacuum block allows for retention of brake booster and map sensor
+
+              For best results we recommend tuning the engine on an aftermarket ECU with a variable TPS sensor.  Units are carefully assembled and thoroughly checked but will require an experienced mechanic to install, set up and tune correctly.
+          `,
+          price         : 39.99,
+          images        : ['/_mock/part5.JPG', '/_mock/part6.JPG', '/_mock/part1.JPG'],
+          specifications: ['Made from premium materials', 'Lasts a lifetime', 'Eco-friendly', 'Comes in multiple colors'],
+          brand         : 'Toyota',
+          stock         : 10,
+          category      : 'Automotive',
+          reviews       : [],
+          rating        : 0,
+          numReviews    : 0
+        },
+        {
+          id            : 4,
+          model         : '[EP70/71] Toyota Starlet',
+          name          : "'INTERCOOLER TURBO' Badge",
+          title         : 'Rare part reimagined!',
+          description   : 'This is a reproduction of the rare and coveted ‘Intercooler Turbo’ badge found on some EP71 Turbo S Starlets.  Through redesigning this badge we have added captive threads instead of the screws of the original design to assure it stands the test of time.  Constructed from tough ABS plastic.',
+          price         : 1149.99,
+          images        : ['/_mock/part1.JPG', '/_mock/part1.JPG', '/_mock/part3.JPG'],
+          specifications: ['Made from premium materials', 'Lasts a lifetime', 'Eco-friendly', 'Comes in multiple colors'],
+          brand         : 'Toyota',
+          stock         : 10,
+          category      : 'Automotive',
+          reviews       : [],
+          rating        : 0,
+          numReviews    : 0
+        },
+        {
+          id            : 5,
+          model         : '[EP70/71] Toyota Starlet',
+          name          : 'TOYOTA Badge',
+          title         : 'Rare part reimagined!',
+          description   : 'This is a reproduction of the ‘TOYOTA’ badge found on the pre-facelift EP70/71 Starlets.  Through redesigning this badge we have added captive threads instead of the screws of the original design to assure it stands the test of time.  Constructed from tough ABS plastic.',
+          price         : 149.99,
+          images        : ['/_mock/part1.JPG', '/_mock/part3.JPG', '/_mock/part4.JPG'],
+          specifications: ['Made from premium materials', 'Lasts a lifetime', 'Eco-friendly', 'Comes in multiple colors'],
+          brand         : 'Toyota',
+          stock         : 10,
+          category      : 'Automotive',
+          reviews       : [],
+          rating        : 0,
+          numReviews    : 0
+        },
+        {
+          id            : 6,
+          model         : '[EP70/71] Toyota Starlet',
+          name          : "'SI' Badge",
+          title         : 'Affordable reproduction badge!',
+          description   : 'This is a reproduction of the ‘TOYOTA’ badge found on the facelifted EP70/71 Starlets.  Through redesigning this badge we have added captive threads instead of the screws of the original design to assure it stands the test of time.  Constructed from tough ABS plastic.',
+          price         : 50.99,
+          images        : ['/_mock/part3.JPG', '/_mock/part4.JPG', '/_mock/part5.JPG'],
+          specifications: ['Made from premium materials', 'Lasts a lifetime', 'Eco-friendly', 'Comes in multiple colors'],
+          brand         : 'Toyota',
+          stock         : 10,
+          category      : 'Automotive',
+          reviews       : [],
+          rating        : 0,
+          numReviews    : 0
+        },
+        {
+          id            : 7,
+          model         : '[EP70/71] Toyota Starlet',
+          name          : "'TEST_SI' Badge",
+          title         : 'Affordable reproduction badge!',
+          description   : 'This is a reproduction of the ‘TOYOTA’ badge found on the facelifted EP70/71 Starlets.  Through redesigning this badge we have added captive threads instead of the screws of the original design to assure it stands the test of time.  Constructed from tough ABS plastic.',
+          price         : 50.99,
+          images        : ['/_mock/part4.JPG', '/_mock/part5.JPG', '/_mock/part1.JPG'],
+          specifications: ['Made from premium materials', 'Lasts a lifetime', 'Eco-friendly', 'Comes in multiple colors'],
+          brand         : 'Toyota',
+          stock         : 10,
+          category      : 'Automotive',
+          reviews       : [],
+          rating        : 0,
+          numReviews    : 0
+        },
+        {
+          id         : 8,
+          model      : '2EE Toyota',
+          name       : 'ITB Kit',
+          title      : 'Quad throttle supreme noise!',
+          description: `
+              This Individual throttle body kit is designed as a bolt on unit for the Toyota 2EE Engine.  It sports all the necessary components to get your 2EE converted to ITBS!
+                  -40mm Throttle bodies
+                  -Carbon Fibre Nylon runners
+                  -Aluminium cylinder head flange
+                  -Your choice of Carbon fibre Nylon or Chrome plated velocity stacks
+                  -Linkages and throttle arms allow for fine adjustment of butterflies
+                  -Vacuum block allows for retention of brake booster and map sensor
+
+              For best results we recommend tuning the engine on an aftermarket ECU with a variable TPS sensor.  Units are carefully assembled and thoroughly checked but will require an experienced mechanic to install, set up and tune correctly.
+          `,
+          price         : 39.99,
+          images        : ['/_mock/part5.JPG', '/_mock/part6.JPG', '/_mock/part1.JPG'],
+          specifications: ['Made from premium materials', 'Lasts a lifetime', 'Eco-friendly', 'Comes in multiple colors'],
+          brand         : 'Toyota',
+          stock         : 10,
+          category      : 'Automotive',
+          reviews       : [],
+          rating        : 0,
+          numReviews    : 0
+        },
+        {
+          id            : 9,
+          model         : '[EP70/71] Toyota Starlet',
+          name          : "'INTERCOOLER TURBO' Badge",
+          title         : 'Rare part reimagined!',
+          description   : 'This is a reproduction of the rare and coveted ‘Intercooler Turbo’ badge found on some EP71 Turbo S Starlets.  Through redesigning this badge we have added captive threads instead of the screws of the original design to assure it stands the test of time.  Constructed from tough ABS plastic.',
+          price         : 1149.99,
+          images        : ['/_mock/part1.JPG', '/_mock/part1.JPG', '/_mock/part3.JPG'],
+          specifications: ['Made from premium materials', 'Lasts a lifetime', 'Eco-friendly', 'Comes in multiple colors'],
+          brand         : 'Toyota',
+          stock         : 10,
+          category      : 'Automotive',
+          reviews       : [],
+          rating        : 0,
+          numReviews    : 0
+        }
+      ]
+
+}
