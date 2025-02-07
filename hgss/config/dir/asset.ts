1fdcfe3554658c064ac2ee797dd55ac43a5f7853
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-07 18:38:07 +1300

    [feat]: add alternative logo asset to ASSET_DIR configuration

--- hgss/config/dir/asset.ts
+++ hgss/config/dir/asset.ts
@@ -5,7 +5,8 @@ import { connect, connectUrl } from "lib/util/connect"
 
 export const ASSET_DIR = {
   LOGO               : connect('asset', 'logo', 'logo.svg'),
-  LOGO_LIGHT          : connect('image', 'svg', 'vieux-carre-white.svg'),
+  LOGO_ALT           : connect('asset', 'logo', 'logo-alt.svg'),
+  LOGO_LIGHT         : connect('image', 'svg', 'vieux-carre-white.svg'),
   LOGO_PRODUCTION    : connectUrl(GLOBAL.SERVER_URL, 'image/svg/vieux-carre-red.svg'),
   LOGO_PRODUCTION_PNG: connectUrl(GLOBAL.SERVER_URL, 'image/png/vieux-carre-red.png'),
   PROMO              : connect('image', 'promo', 'promo.png'),
