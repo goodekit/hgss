9462edf233d6a3c64a2be6c64f6cd4c52c9ccfd6
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-07 18:53:19 +1300

    [refax]: update Homepage component to use NAV_CONFIG for navigation and improve layout

--- hgss/app/(root)/page.tsx
+++ hgss/app/(root)/page.tsx
@@ -2,32 +2,28 @@ import { Fragment } from 'react'
 // import { GLOBAL } from 'hgss'
 import Image from 'next/image'
 import { ASSET_DIR } from 'hgss-dir'
+import { NAV_CONFIG } from 'hgss-nav'
 // import { getLatestProducts, getAllFeaturedProducts } from 'lib'
-import { LinkBtn } from 'component/shared/btn'
-import { DealCountdown } from 'component/shared/promo'
-import { ServiceCard } from 'component/shared/card'
-import { getRandomTextureClass } from 'lib/util'
+import { getRandomTextureClass, delay } from 'lib/util'
 
 const Homepage = async () => {
 //   const latestProducts   = await getLatestProducts()
 //   const featuredProducts = await getAllFeaturedProducts()
-const randomizedClasses = Object.values(TAB).map(() => getRandomTextureClass())
+
+const randomizedClasses = NAV_CONFIG.map(() => getRandomTextureClass())
   return (
     <Fragment>
-    <div className="min-h-screen p-4 flex flex-col items-center relative justify-between">
+    <div className="p-4 flex flex-col items-center relative justify-between">
         <div className="w-full max-w-4xl mt-20">
           <div className="w-full rounded-lg pb-10 p-5 relative mb-24">
-            <Image src={ASSET_DIR.LOGO} priority alt="Homegrown Logo" width={800} height={300} className="m-auto p-12 bg-blend-hard-light" />
+            <Image src={ASSET_DIR.LOGO} priority alt="Homegrown Logo" width={800} height={300} className="m-auto p-12 " />
           </div>
 
-          <nav className="space-y-4 permanent-marker-regular">
-            {Object.values(TAB).map((item, index) => (
-              <LinkBtn
-                key={index}
-                className={`w-full text-4xl xs:text-xl permanent-marker-regular ${randomizedClasses[index]} text-black font-bold rounded transform -rotate-1 hover:rotate-0 transition-transform shadow shadow-md`}
-                href={`/${item}`}>
-                {item}
-              </LinkBtn>
+          <nav className="permanent-marker-regular ">
+            {NAV_CONFIG.map(({ title, href }, index) => (
+              <button key={index} className={`w-full  text-4xl xs:text-xl permanent-marker-regular ${randomizedClasses[index]} text-black font-bold transform -rotate-1 hover:rotate-0 transition-transform`}>
+                <a href={href}>{title}</a>
+              </button>
             ))}
           </nav>
         </div>
