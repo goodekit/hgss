e1ffa48f135e363e754802289b337f47fb3dc030
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-08 18:54:19 +1300

    [refax]: update NotFoundPage styles with custom font and adjust heading sizes

--- hgss/app/not-found.tsx
+++ hgss/app/not-found.tsx
@@ -13,10 +13,10 @@ const NotFoundPage = () => {
     <div className="flex flex-col items-center justify-center min-h-screen shadow-none">
       <Image src={ASSET_DIR.LOGO_ALT} width={60} height={60} alt="logo" priority />
       <div className="p-6 w-1/3 rounded-sm text-center">
-        <h4 className="text-lg mt-2">{CODE.NOT_FOUND}</h4>
+        <h4 className="text-4xl mt-2 covered-by-your-grace-regular">{CODE.NOT_FOUND}</h4>
         <h1 className="text-3xl font-bold mb-4">{HTTP_RESPONSE[404].title}</h1>
-        <h6 className="mt-8">{HTTP_RESPONSE[404].description}</h6>
-        <button className={`w-1/4 mt-4 ml-2 text-black w-full  ${getRandomTextureClass()} transform -rotate-1 hover:rotate-0 transition-transform`} onClick={handleNavigateHome}>
+        <h6 className="mt-8 covered-by-your-grace-regular">{HTTP_RESPONSE[404].description}</h6>
+        <button className={`w-1/4 mt-4 ml-2 text-xl  text-black w-full  ${getRandomTextureClass()} transform -rotate-1 hover:rotate-0 transition-transform`} onClick={handleNavigateHome}>
           {en.go_back}
         </button>
       </div>
