b78239afed392377df40cb505c85409e410195cc
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-09 23:27:29 +1300

    [refax]: ProductImage component layout for improved responsiveness and user interaction

--- hgss/component/module/product/product-image.tsx
+++ hgss/component/module/product/product-image.tsx
@@ -10,17 +10,19 @@ interface ProductImageProps {
 const ProductImage: FC<ProductImageProps> = ({ images }) => {
   const [current, setCurrent] = useState(0)
   return (
-    <div className="space-y-4">
-      <Image src={images[current]} alt={'product-image'} width={1000} height={1000} className="min-h-[300px] object-cover object-center" priority />
-      <div className="flex">
+    <div className="grid grid-cols-1 md:grid-cols-7 gap-4 h-[500px]">
+       <div className="col-span-1">
         {images.map((image, index) => (
           <div
             key={index}
             onClick={() => setCurrent(index)}
-            className={cn('border-b-4 mr-2 cursor-pointer hover:border-gray-700', current === index && 'border-gray-400')}>
+            className={cn('cursor-pointer hover:opacity-50 ease-in-out transition', index !== 0 && 'mt-2')}>
             <Image src={image} alt={'product-image'} width={80} height={80} className={cn(current !== index && 'scale-75')} />
           </div>
         ))}
+      </div>
+      <div className={'col-span-6'}>
+        <Image src={images[current]} alt={'product-image'} width={1000} height={1000} className="min-h-[300px] object-cover object-center" priority />
       </div>
     </div>
   )
