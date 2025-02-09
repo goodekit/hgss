dfcae77b4b2289c6a2cb41fbd4fe63b1213eb1d5
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-09 23:00:35 +1300

    [db-mock]: enhance pre-load script to clear and seed user data with hashed passwords

--- hgss/_mock/pre-load.ts
+++ hgss/_mock/pre-load.ts
@@ -1,12 +1,26 @@
 import { PrismaClient } from '@prisma/client'
 import goodlog from 'good-logs'
+import { hash } from 'lib'
 import { _mockData } from './_mock-data'
 
 
 async function main() {
     const prisma = new PrismaClient()
     await prisma.product.deleteMany()
+    await prisma.account.deleteMany()
+    await prisma.session.deleteMany()
+    await prisma.verificationToken.deleteMany()
+    await prisma.user.deleteMany()
+
     await prisma.product.createMany({ data: _mockData.products })
+    const users = []
+    for (let i = 0; i < _mockData.users.length; i++) {
+      users.push({
+        ..._mockData.users[i],
+        password: await hash(_mockData.users[i].password),
+      })
+    }
+    await prisma.user.createMany({ data: users });
     goodlog.log(' 🌱 Data pre-loaded')
 }
 
