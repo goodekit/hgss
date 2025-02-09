57ce763155cafbcdcd7f6d01e3e6c58c30dfcfa5
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-09 23:34:33 +1300

    [feat]: add AuthLayout component for authentication page layout

---
+++ hgss/app/(auth)/layout.tsx
@@ -0,0 +1,5 @@
+import { ReactNode } from 'react'
+
+export default function AuthLayout({ children }: Readonly<{ children: ReactNode }>) {
+  return <div className="flex-center min-h-screen w-full">{children}</div>
+}
