51c499d511de8b0448b9ac266b0d2646eac53b00
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-11 23:14:59 +1300

    activate bag action

--- component/shared/header/toolbar.tsx
+++ component/shared/header/toolbar.tsx
@@ -1,10 +1,9 @@
-// import { getMyBagCount } from 'lib/action'
+import { getMyBagCount } from 'lib/action'
 import { ThemeToggle, MobileMenu, BagIconWithBadge } from 'component/shared'
 import { UserMenu } from 'component/shared/header'
 
 const Toolbar = async () => {
-  // const count = await getMyBagCount()
-  const count = 5
+  const count = await getMyBagCount()
   return (
     <div className="flex justify-end gap-3">
       <nav className="hidden md:flex w-full max-w-xs gap-1">
