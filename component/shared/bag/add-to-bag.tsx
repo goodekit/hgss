eae1af1e54d70809cec3b50b19f3d4b418339d35
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-11 23:04:02 +1300

    activated the bag page

--- component/shared/bag/add-to-bag.tsx
+++ component/shared/bag/add-to-bag.tsx
@@ -4,16 +4,15 @@ import { Fragment, useTransition, FC } from 'react'
 import { useRouter } from 'next/navigation'
 import { en } from 'public/locale'
 import { PATH_DIR } from 'hgss-dir'
-import { Plus } from 'lucide-react'
 import { useToast } from 'hook'
-// import { addItemToBag, removeItemFromBag } from 'lib/action'
-import { Button, ToastAction } from 'component/ui'
-import { DynamicBagBtn } from 'component/shared/btn'
+import { addItemToBag, removeItemFromBag } from 'lib/action'
+import { ToastAction } from 'component/ui'
+import { DynamicBagBtn, TapeBtn } from 'component/shared/btn'
 import { EllipsisLoader } from 'component/shared/loader'
 
 interface AddToBagProps {
-  item: BagItem
-  bag?: Bag
+  item : BagItem
+  bag ?: Bag
 }
 
 const AddToBag: FC<AddToBagProps> = ({ bag, item }) => {
@@ -24,42 +23,40 @@ const AddToBag: FC<AddToBagProps> = ({ bag, item }) => {
 
   const handleAddToBag = async () => {
     startTransition(async () => {
-      // const response = await addItemToBag(item)
-      // if (!response.success) {
-      //   toast({ variant: 'destructive', description: response.message })
-      //   return
-      // }
-      // toast({
-      //   description: response.message,
-      //   action: (
-      //     <ToastAction className="bg-primary text-white hover:bg-gray-800" altText="Go to Bag" onClick={() => router.push(PATH_DIR.BAG)}>
-      //       {en.go_to_bag}
-      //     </ToastAction>
-      //   )
-      // })
+      const response = await addItemToBag(item)
+      if (!response.success) {
+        toast({ variant: 'destructive', description: response.message })
+        return
+      }
+      toast({
+        description: response.message,
+        action: (
+          <ToastAction className="bg-primary text-white hover:bg-gray-800" altText="Go to Bag" onClick={() => router.push(PATH_DIR.BAG)}>
+            {en.go_to_bag}
+          </ToastAction>
+        )
+      })
     })
   }
 
-  // const handleRemoveFromBag = async () => {
-  //   startTransition(async () => {
-  //     const response = await removeItemFromBag(item.productId)
-  //     toast({ variant: response.success ? 'default' : 'destructive', description: response.message })
-  //     return
-  //   })
-  // }
+  const handleRemoveFromBag = async () => {
+    startTransition(async () => {
+      const response = await removeItemFromBag(item.productId)
+      toast({ variant: response.success ? 'default' : 'destructive', description: response.message })
+      return
+    })
+  }
 
   const render = existItem ? (
-    <DynamicBagBtn isPending={isPending} handlePlus={()=> {}} handleMinus={()=> {}} amount={existItem.qty} />
+    <DynamicBagBtn isPending={isPending} handlePlus={handleAddToBag} handleMinus={handleRemoveFromBag} amount={existItem.qty} />
   ) : (
-    <Button className="w-full rounded-sm" type={'button'} onClick={()=> {}}>
-      {isPending ? (
-        <EllipsisLoader />
-      ) : (
-        <Fragment>
-          <Plus /> {en.add_to_bag}
-        </Fragment>
-      )}
-    </Button>
+    <TapeBtn type={'button'} onClick={handleAddToBag} label={isPending ? (
+    <EllipsisLoader />
+    ) : (
+      <Fragment>
+      {'+'} {en.add_to_bag}
+      </Fragment>
+    )}/>
   )
   return render
 }
