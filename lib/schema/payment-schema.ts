09546b474b3c96a6c1675460dd6cb9cea89fc258
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-12 23:30:00 +1300

    [schema]: payment schema

--- lib/schema/payment-schema.ts
+++ lib/schema/payment-schema.ts
@@ -3,11 +3,10 @@ import { en } from "public/locale"
 import { z } from "zod"
 import { KEY } from "lib/constant"
 
-export const PAYMENT_METHODS = GLOBAL.PAYMENT_METHODS && GLOBAL.PAYMENT_METHODS.split(';') || ['PayPal', 'Stripe', 'CashOnDelivery']
+export const PAYMENT_METHODS = GLOBAL.PAYMENT_METHODS && GLOBAL.PAYMENT_METHODS.split(';') || ['PayPal', 'Stripe']
 
 export const PAYPAL           = PAYMENT_METHODS?.find((method) => method === 'PayPal') || KEY.PAYPAL
 export const STRIPE           = PAYMENT_METHODS?.find((method) => method === 'Stripe') || KEY.STRIPE
-export const CASH_ON_DELIVERY = PAYMENT_METHODS?.find((method) => method === 'CashOnDelivery') || KEY.CASH_ON_DELIVERY
 
 export const PaymentMethodSchema = z.object({
     type: z.string().min(1, 'Payment method is required')
