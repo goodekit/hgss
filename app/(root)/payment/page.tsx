f7566539ee1900f1057ec21347656c17a3c4f44a
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-12 23:26:30 +1300

    [feat]: added payment form

---
+++ app/(root)/payment/page.tsx
@@ -0,0 +1,24 @@
+import { Fragment } from 'react'
+import { Metadata } from 'next'
+import { en } from 'public/locale'
+import { auth } from 'auth'
+import { getUserById } from 'lib/action'
+import { PurchaseFlow } from 'component/shared/custom'
+import PaymentForm from './payment-form'
+
+export const metadata: Metadata = { title: 'Payment' }
+
+const PaymentPage = async () => {
+  const session = await auth()
+  const userId  = session?.user?.id
+  if (!userId) throw new Error(en.error.user_not_found)
+  const user = await getUserById(userId)
+  return (
+    <Fragment>
+      <PurchaseFlow current={2} locale={en} />
+      <PaymentForm paymentMethod={user.paymentMethod} />
+    </Fragment>
+  )
+}
+
+export default PaymentPage
