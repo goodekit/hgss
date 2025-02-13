3c89a7703f17b3738cc4f26d936a5ddffa6a9177
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-14 10:54:01 +1300

    consistency with the buttons

--- app/(root)/order/checkout/checkout-form.tsx
+++ app/(root)/order/checkout/checkout-form.tsx
@@ -1,11 +1,11 @@
 'use client'
-import { FormEvent } from 'react'
+import { FormEvent, Fragment } from 'react'
 import { en } from 'public/locale'
 import { useRouter } from 'next/navigation'
 import { useFormStatus } from 'react-dom'
 import { createOrder } from 'lib/action'
 import { Barcode } from 'lucide-react'
-import { Button } from 'component/ui'
+import { TapeBtn } from 'component/shared/btn'
 import { EllipsisLoader } from 'component/shared/loader'
 
 const CheckoutForm = () => {
@@ -19,10 +19,17 @@ const CheckoutForm = () => {
   const CheckoutButton = () => {
     const { pending } = useFormStatus()
     return (
-      <Button disabled={pending} className={'w-full'}>
-        {pending ? <EllipsisLoader /> : <Barcode className={'default-size_icon'} />}
-        {pending ? <i>{en.place_order.pending}</i> : en.place_order.label}
-      </Button>
+      <TapeBtn
+        label={
+          <Fragment>
+            {pending ? <EllipsisLoader /> : <Barcode className={'default-size_icon'} />}
+            {pending ? <i>{en.place_order.pending}</i> : en.place_order.label}
+          </Fragment>
+        }
+        disabled={pending}
+        textSize={'text-xl'}
+        className={'w-full'}
+      />
     )
   }
   return (
