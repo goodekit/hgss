c0194ff6a9aee923740b846b674cc7a986d4b65d
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-14 11:00:30 +1300

    added error handler

--- lib/error-handler.ts
+++ lib/error-handler.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-// import { Prisma } from '@prisma/client'
+import { Prisma } from '@prisma/client'
 import { ZodError } from 'zod'
 
 
@@ -7,6 +7,9 @@ export const errorHandler = (error: AppError) => {
   if (error instanceof ZodError) {
     const errors = error.errors.map((err) => err.message)
     return errors.join(', ')
+  } else if (error instanceof Prisma.PrismaClientKnownRequestError) {
+    const field = (error.meta as { target?: string[] })?.target ? (error.meta as { target?: string[] }).target![0] : 'Field'
+    return `${field.charAt(0).toUpperCase() + field.slice(1)} already exists`
   } else {
     return typeof error.message === 'string' ? error.message : JSON.stringify(error.message)
   }
