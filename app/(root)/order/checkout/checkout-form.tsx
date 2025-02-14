1948d5978f74bbe4f10f76b583e2a0134a05fdde
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-15 03:19:26 +1300

    [ui]:pending state refax

--- app/(root)/order/checkout/checkout-form.tsx
+++ app/(root)/order/checkout/checkout-form.tsx
@@ -1,10 +1,10 @@
 'use client'
+
 import { FormEvent, Fragment } from 'react'
 import { en } from 'public/locale'
 import { useRouter } from 'next/navigation'
 import { useFormStatus } from 'react-dom'
 import { createOrder } from 'lib/action'
-import { Barcode } from 'lucide-react'
 import { TapeBtn } from 'component/shared/btn'
 import { EllipsisLoader } from 'component/shared/loader'
 
@@ -22,8 +22,7 @@ const CheckoutForm = () => {
       <TapeBtn
         label={
           <Fragment>
-            {pending ? <EllipsisLoader /> : <Barcode className={'default-size_icon'} />}
-            {pending ? <i>{en.place_order.pending}</i> : en.place_order.label}
+            {pending ? <div className={'flex'}><i>{en.place_order.pending}</i><span><EllipsisLoader /></span></div> : en.place_order.label}
           </Fragment>
         }
         disabled={pending}
