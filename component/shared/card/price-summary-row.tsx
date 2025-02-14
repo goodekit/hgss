5d8d9a201c42b8fc3aa09f7d1eaa8b26b18e83f4
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-14 15:12:02 +1300

    line through for promotions

--- component/shared/card/price-summary-row.tsx
+++ component/shared/card/price-summary-row.tsx
@@ -1,16 +1,31 @@
-import { FC } from "react"
-import { formatCurrency } from "lib/util"
+import { FC, Fragment } from "react"
+import { formatCurrency, cn } from "lib/util"
 
 interface PriceSummaryRowProps {
-  label     : string
-  price     : string | number
-  className?: string
+  label            : string
+  price            : string | number
+  className       ?: string
+  hasPromotion    ?: boolean
+  promotionalPrice?: string | number
 }
-const PriceSummaryRow: FC<PriceSummaryRowProps> = ({ label, price, className }) => {
+const PriceSummaryRow: FC<PriceSummaryRowProps> = ({ label, price, className, hasPromotion, promotionalPrice }) => {
   return (
-    <div className="flex justify-between">
+    <div className="flex justify-between items-center">
       <div className={className}>{label}</div>
-      <div className={className}>{formatCurrency(price)}</div>
+      <div className="flex flex-col items-end">
+        {hasPromotion ? (
+          <Fragment>
+            <div className="text-gray-500 line-through text-sm">
+              {formatCurrency(price)}
+            </div>
+            <div className={cn("text-green-600", className)}>
+              {promotionalPrice && formatCurrency(promotionalPrice)}
+            </div>
+          </Fragment>
+        ) : (
+          <div className={className}>{formatCurrency(price)}</div>
+        )}
+      </div>
     </div>
   )
 }
