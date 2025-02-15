1620f763a3f0fcdc0221eb8d57bdec1bd01a002b
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-16 03:39:24 +1300

    enhance tape button

--- component/shared/btn/tape-btn.tsx
+++ component/shared/btn/tape-btn.tsx
@@ -1,6 +1,9 @@
-import React, { FC, JSX } from "react"
+"use client"
+
+import React, { FC, Fragment, JSX } from "react"
 import { NAV_CONFIG } from "hgss-nav"
 import { getRandomTextureClass, cn } from "lib"
+import { EllipsisLoader } from 'component/shared/loader'
 
 interface TapeBtnProps {
   label     : JSX.Element | string
@@ -8,17 +11,21 @@ interface TapeBtnProps {
   className?: string
   type     ?: ButtonType
   index    ?: number
+  isPending?: boolean
   disabled ?: boolean
   isLink   ?: boolean
   textSize ?: string
+  icon     ?: JSX.Element |string
   onClick  ?: () => void
 }
 
-const TapeBtn: FC<TapeBtnProps> = ({ href, label, className, index = 2, disabled, type = 'submit', isLink = false, onClick, textSize }) => {
- const randomizedClasses = NAV_CONFIG.map(() => getRandomTextureClass())
+const TapeBtn: FC<TapeBtnProps> = ({ href, label, className, index = 2, disabled, type = 'submit', isLink = false, onClick, textSize, icon, isPending }) => {
+ const randomizedClasses  = NAV_CONFIG.map(() => getRandomTextureClass())
+ const renderPendingState = isPending ? ( <EllipsisLoader /> ) : ( <Fragment> {label} {icon} </Fragment> )
+
   return (
     <button disabled={disabled} type={type} className={cn(`w-full xs:text-xl permanent-marker-regular text-black font-bold transform -rotate-1 hover:rotate-0 transition-transform`, randomizedClasses[index], textSize ? textSize : 'text-4xl',  className)} onClick={onClick}>
-     {isLink ? <a href={href}>{label}</a> : label}
+     {isLink ? <a href={href}>{renderPendingState}</a> : renderPendingState}
     </button>
   )
 }
