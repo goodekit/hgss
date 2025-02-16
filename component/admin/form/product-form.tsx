634ef1e46898c3da23761d772ecbe92eede2a208
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-16 20:36:15 +1300

    [feat]: form component for product added

---
+++ component/admin/form/product-form.tsx
@@ -0,0 +1,149 @@
+'use client'
+
+import { FC, Fragment, useEffect } from 'react'
+import { en } from 'public/locale'
+import { PATH_DIR } from 'hgss-dir'
+import { useRouter } from 'next/navigation'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { SubmitHandler, useForm } from 'react-hook-form'
+import { useToast, usePreventNavigation, useFormState } from 'hook'
+import slugify from 'slugify'
+import { Plus, MoveUpRight } from 'lucide-react'
+import { ProductSchema, UpdateProductSchema, productDefaultValue } from 'lib/schema'
+import { createProduct, updateProduct } from 'lib/action'
+import { Form } from 'component/ui'
+import { BannerUploadField } from 'component/admin/custom-field'
+import { LoadingBtn } from 'component/shared/btn'
+import { RHFFormField, RHFFormDropzone, RHFCheckbox } from 'component/shared/rhf'
+import { capitalize , delay } from 'lib/util'
+
+const ProductForm: FC<ProductForm> = ({ type, product, productId }) => {
+  const { toast }             = useToast()
+  const { isDirty, setDirty } = useFormState()
+  const router                = useRouter()
+  const form                  = useForm<Product>({
+    resolver: type === 'update' ? zodResolver(UpdateProductSchema) : zodResolver(ProductSchema),
+    defaultValues: product && type === 'update' ? product : productDefaultValue
+  })
+
+  const { control, formState, handleSubmit } = form
+  const images                               = form.watch('images')
+  const isFeatured                           = form.watch('isFeatured')
+  const banner                               = form.watch('banner')
+
+  // const { fields, append, remove } = useFieldArray<string[]>({ control, name: 'specifications' })
+
+  usePreventNavigation(isDirty)
+
+  useEffect(() => {
+    setDirty(formState.isDirty)
+  }, [formState.isDirty, setDirty])
+
+  // useEffect(() => {
+  //   const specs = form.watch('specifications')
+  //   if (typeof specs === 'string') {
+  //     const parsedSpecs = specs && specs.split(',').map(s => s.trim()).filter(Boolean) // Remove spaces & empty values
+  //     form.setValue('specifications', parsedSpecs)
+  //   }
+  // }, [form.watch('specifications')])
+
+  const handleSlugify = () => {
+    form.setValue('slug', slugify(form.getValues('name'), { lower: true }))
+  }
+
+  const onSubmit: SubmitHandler<CreateProduct> = async (data) => {
+    await delay(500)
+    if (type === 'create') {
+      const response = await createProduct(data)
+      if (!response.success) {
+        toast({ variant: 'destructive', description: response.message })
+      } else {
+        toast({ description: response.message })
+      }
+      router.push(PATH_DIR.ADMIN.PRODUCT)
+    }
+
+    if (type === 'update') {
+      if (!productId) {
+        toast({ variant: 'destructive', description: en.error.product_not_found })
+        router.push(PATH_DIR.ADMIN.PRODUCT)
+        return
+      }
+      const response = await updateProduct({ ...data, id: productId })
+      if (!response.success) {
+        toast({ variant: 'destructive', description: response.message })
+      } else {
+        toast({ description: response.message })
+      }
+      router.push(PATH_DIR.ADMIN.PRODUCT)
+    }
+  }
+
+  return (
+    <Fragment>
+      <Form {...form}>
+        <form method={'POST'} onSubmit={handleSubmit(onSubmit)} className="space-y-8">
+          <div className="flex flex-col md:flex-row gap-4">
+            <RHFFormField control={control} name={'name'} formKey={'name'} withWrapper={false} />
+            <RHFFormField
+              control={control}
+              name={'slug'}
+              formKey={'slug'}
+              type={'inputWithButton'}
+              withWrapper={false}
+              buttonLabel={en.generate.label}
+              onClick={handleSlugify}
+            />
+          </div>
+          <div className="flex flex-col md:flex-row gap-4">
+            <RHFFormField control={control} name={'category'} formKey={'category'} withWrapper={false} />
+            <RHFFormField control={control} name={'brand'} formKey={'brand'} withWrapper={false} />
+          </div>
+          <div className="flex flex-col md:flex-row gap-4">
+            <RHFFormField control={control} name={'price'} formKey={'price'} withWrapper={false} />
+            <RHFFormField control={control} name={'stock'} formKey={'stock'} withWrapper={false} />
+          </div>
+          <div className=" flex flex-col md:flex-row gap-4">
+            {/* <RHFFormField control={control} name={'specifications'} formKey={'stock'} withWrapper={false}  /> */}
+            {/* <div className="flex flex-col gap-4">
+                <label>Specifications</label>
+                {fields.map((field, index) => (
+                  <div key={field.id} className="flex gap-2">
+                    <input
+                      {...register(`specifications.${index}` as const)}
+                      className="border p-2 rounded w-full"
+                    />
+                    <Button type="button" onClick={() => remove(index)}>Remove</Button>
+                  </div>
+                ))}
+                <Button type="button" onClick={() => append('')}>+ Add Specification</Button>
+              </div> */}
+          </div>
+          <div className="upload-field flex flex-col md:flex-row gap-4">
+            <RHFFormDropzone control={control} name={'images'} formKey={'images'} images={images} form={form} />
+          </div>
+          <div className="upload-field gap-4 h-auto">
+              <RHFFormField control={control} name={'description'} formKey={'description'} type={'textarea'} withWrapper={false} />
+          </div>
+          <div className="upload-field gap-4 h-auto">
+              <RHFCheckbox control={control} name={'isFeatured'} formKey={'featured'} />
+          </div>
+          <div className="upload-field gap-4">
+            <BannerUploadField isFeatured={isFeatured} banner={banner!} onClientUploadComplete={(res: { url: string }[]) => { form.setValue('banner', res[0].url ) }}  />
+          </div>
+          <div className="flex justify-end">
+            <LoadingBtn
+              isPending={formState.isSubmitting}
+              label={`${capitalize(type)} Product`}
+              disabled={formState.isSubmitting}
+              className={'min-w-24 sm:w-full'}
+              icon={type === 'create' ? <Plus size={20} /> : <MoveUpRight size={20} />}
+            />
+          </div>
+        </form>
+      </Form>
+    </Fragment>
+  )
+}
+
+export default ProductForm
