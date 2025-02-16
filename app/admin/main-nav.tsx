9c38c59d6ed6beed8a3cbd4667c6f964a7a3eb53
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-16 20:29:59 +1300

    [feat]: admin page added

---
+++ app/admin/main-nav.tsx
@@ -0,0 +1,19 @@
+"use client"
+import { HTMLAttributes } from 'react'
+import { NAV_CONFIG_ADMIN } from 'hgss-nav'
+import { cn } from 'lib'
+import { ProtectedNavLink } from 'component/shared/protect'
+
+const MainNav = ({ className, ...props }: HTMLAttributes<HTMLElement>) => {
+  return (
+    <nav className={cn('flex items-center space-x-4 lg:space-x-6', className)} {...props}>
+      {NAV_CONFIG_ADMIN.map((item, index) => (
+        <ProtectedNavLink key={index} href={item.href}>
+          {item.title}
+        </ProtectedNavLink>
+      ))}
+    </nav>
+  )
+}
+
+export default MainNav
