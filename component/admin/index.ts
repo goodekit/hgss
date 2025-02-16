31f21752a1ac073452a3c96963162caa96b54d1b
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-16 20:37:49 +1300

    impor traces

---
+++ component/admin/index.ts
@@ -0,0 +1,9 @@
+// ⎿ custom-field
+export { default as BannerUploadField } from './custom-field/banner-upload-field'
+// ⎿ form
+export { default as ProductForm } from './form/product-form'
+export { default as UserAccountForm } from './form/user-account-form'
+// ⎿ search
+export { default as AdminSearch } from './search/admin-search'
+// ⎿ title
+export { default as PageTitle } from './title/page-title'
