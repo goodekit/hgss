93da6071d9397dfef204a6d94dc782267a4e6c0d
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-16 20:38:32 +1300

    [design-consistency]: arrow icon updated

--- component/shared/btn/form-back-btn.tsx
+++ component/shared/btn/form-back-btn.tsx
@@ -1,7 +1,6 @@
 "use client"
 import { FC } from 'react'
 import { en } from 'public/locale'
-import { ArrowLeft } from 'lucide-react'
 import { Tooltp } from 'component/shared/tooltp'
 import { Button } from 'component/ui'
 import { ProtectedNavLink } from 'component/shared/protect'
@@ -23,14 +22,14 @@ const FormBackBtn: FC<FormBackBtnProps> = ({ href = '', label,  handleExclusiveB
         {withLink ? (
           <Button variant={'ghost'}>
             <ProtectedNavLink href={href} className={'flex flex-row items-center gap-2'}>
-              <ArrowLeft className={'default-size_icon'} />  &nbsp; <span>{label}</span>
+              <p className={'text-xl text-tape'}>{'<-'}</p>  &nbsp; <span>{label}</span>
             </ProtectedNavLink>
           </Button>
         )
         :
         (
           <Button variant={'ghost'} onClick={handleBackButton || handleExclusiveBack}>
-            <ArrowLeft className={'default-size_icon'} />
+            <p className={'text-xl text-tape'}>{'<-'}</p>
           </Button>
         )}
     </Tooltp>
