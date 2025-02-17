9ec3367cf8e044a8dd4629d6c87d9dc4f8d29678
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-18 00:41:11 +1300

    [bugfix]: fixed errors on form

--- component/admin/form/product-form.tsx
+++ component/admin/form/product-form.tsx
@@ -5,17 +5,17 @@ import { en } from 'public/locale'
 import { PATH_DIR } from 'hgss-dir'
 import { useRouter } from 'next/navigation'
 import { zodResolver } from '@hookform/resolvers/zod'
-import { SubmitHandler, useForm } from 'react-hook-form'
+import { SubmitHandler, useForm, useFieldArray } from 'react-hook-form'
 import { useToast, usePreventNavigation, useFormState } from 'hook'
 import slugify from 'slugify'
 import { Plus, MoveUpRight } from 'lucide-react'
 import { ProductSchema, UpdateProductSchema, productDefaultValue } from 'lib/schema'
 import { createProduct, updateProduct } from 'lib/action'
-import { Form } from 'component/ui'
+import { Form, Button } from 'component/ui'
 import { BannerUploadField } from 'component/admin/custom-field'
 import { LoadingBtn } from 'component/shared/btn'
 import { RHFFormField, RHFFormDropzone, RHFCheckbox } from 'component/shared/rhf'
-import { capitalize , delay } from 'lib/util'
+import { capitalize, delay } from 'lib/util'
 
 const ProductForm: FC<ProductForm> = ({ type, product, productId }) => {
   const { toast }             = useToast()
@@ -26,12 +26,13 @@ const ProductForm: FC<ProductForm> = ({ type, product, productId }) => {
     defaultValues: product && type === 'update' ? product : productDefaultValue
   })
 
-  const { control, formState, handleSubmit } = form
+  const { control, formState, register, handleSubmit } = form
+  const { errors }                           = formState
   const images                               = form.watch('images')
   const isFeatured                           = form.watch('isFeatured')
   const banner                               = form.watch('banner')
 
-  // const { fields, append, remove } = useFieldArray<string[]>({ control, name: 'specifications' })
+  const { fields, append, remove } = useFieldArray({ control, name: 'specifications' })
 
   usePreventNavigation(isDirty)
 
@@ -39,45 +40,43 @@ const ProductForm: FC<ProductForm> = ({ type, product, productId }) => {
     setDirty(formState.isDirty)
   }, [formState.isDirty, setDirty])
 
-  // useEffect(() => {
-  //   const specs = form.watch('specifications')
-  //   if (typeof specs === 'string') {
-  //     const parsedSpecs = specs && specs.split(',').map(s => s.trim()).filter(Boolean) // Remove spaces & empty values
-  //     form.setValue('specifications', parsedSpecs)
-  //   }
-  // }, [form.watch('specifications')])
-
   const handleSlugify = () => {
     form.setValue('slug', slugify(form.getValues('name'), { lower: true }))
   }
 
-  const onSubmit: SubmitHandler<CreateProduct> = async (data) => {
-    await delay(500)
-    if (type === 'create') {
-      const response = await createProduct(data)
-      if (!response.success) {
-        toast({ variant: 'destructive', description: response.message })
-      } else {
-        toast({ description: response.message })
+ const onSubmit: SubmitHandler<CreateProduct> = async (data) => {
+  if (Object.keys(errors).length > 0) {
+    toast({ variant: 'destructive', description: 'Please fix form errors before submitting.' });
+    return
+  }
+    try {
+      await delay(500)
+      if (type === 'create') {
+        const response = await createProduct(data)
+        if (!response.success) {
+          toast({ variant: 'destructive', description: response.message })
+        } else {
+          toast({ description: response.message })
+        }
       }
-      router.push(PATH_DIR.ADMIN.PRODUCT)
-    }
 
-    if (type === 'update') {
-      if (!productId) {
-        toast({ variant: 'destructive', description: en.error.product_not_found })
-        router.push(PATH_DIR.ADMIN.PRODUCT)
-        return
-      }
-      const response = await updateProduct({ ...data, id: productId })
-      if (!response.success) {
-        toast({ variant: 'destructive', description: response.message })
-      } else {
-        toast({ description: response.message })
+      if (type === 'update' && productId) {
+        const response = await updateProduct({ ...data, id: productId })
+        if (!response.success) {
+          toast({ variant: 'destructive', description: response.message })
+        } else {
+          toast({ description: response.message })
+        }
       }
       router.push(PATH_DIR.ADMIN.PRODUCT)
+    } catch (error) {
+      toast({
+        variant: 'destructive',
+        description: 'An error occurred while saving the product' + error
+      })
     }
   }
+
 
   return (
     <Fragment>
@@ -103,30 +102,31 @@ const ProductForm: FC<ProductForm> = ({ type, product, productId }) => {
             <RHFFormField control={control} name={'price'} formKey={'price'} withWrapper={false} />
             <RHFFormField control={control} name={'stock'} formKey={'stock'} withWrapper={false} />
           </div>
-          <div className=" flex flex-col md:flex-row gap-4">
-            {/* <RHFFormField control={control} name={'specifications'} formKey={'stock'} withWrapper={false}  /> */}
-            {/* <div className="flex flex-col gap-4">
-                <label>Specifications</label>
+          <div className="flex flex-col md:flex-row gap-4">
+            <RHFFormField control={control} name={'model'} formKey={'model'} withWrapper={false} />
+          </div>
+          <div className=" flex flex-col md:flex-col gap-4">
+            <div className="flex flex-col gap-4">
+                <label>{en.form.specifications.label}</label>
                 {fields.map((field, index) => (
-                  <div key={field.id} className="flex gap-2">
-                    <input
-                      {...register(`specifications.${index}` as const)}
+                  <div key={field.id} className={"flex gap-2"}>
+                    <input {...register(`specifications.${index}` as const)}
                       className="border p-2 rounded w-full"
                     />
-                    <Button type="button" onClick={() => remove(index)}>Remove</Button>
+                    <Button type="button" onClick={() => remove(index)}>{en.remove.label}</Button>
                   </div>
                 ))}
-                <Button type="button" onClick={() => append('')}>+ Add Specification</Button>
-              </div> */}
+                <Button type="button" onClick={() => append('')}>{'+ '}{en.form.specifications.placeholder}</Button>
+              </div>
           </div>
           <div className="upload-field flex flex-col md:flex-row gap-4">
             <RHFFormDropzone control={control} name={'images'} formKey={'images'} images={images} form={form} />
           </div>
           <div className="upload-field gap-4 h-auto">
-              <RHFFormField control={control} name={'description'} formKey={'description'} type={'textarea'} withWrapper={false} />
+            <RHFFormField control={control} name={'description'} formKey={'description'} type={'textarea'} withWrapper={false} />
           </div>
           <div className="upload-field gap-4 h-auto">
-              <RHFCheckbox control={control} name={'isFeatured'} formKey={'featured'} />
+            <RHFCheckbox control={control} name={'isFeatured'} formKey={'featured'} />
           </div>
           <div className="upload-field gap-4">
             <BannerUploadField isFeatured={isFeatured} banner={banner!} onClientUploadComplete={(res: { url: string }[]) => { form.setValue('banner', res[0].url ) }}  />
