c22ea7e2bb0074133d07d774d768ca7e9ed7a249
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-18 03:05:57 +1300

    [design-consistency]

--- component/module/product/product-card.tsx
+++ component/module/product/product-card.tsx
@@ -1,8 +1,12 @@
+"use client"
+
 import { FC } from 'react'
 import { PATH_DIR } from 'hgss-dir'
 import Link from 'next/link'
 import Image from 'next/image'
+import { useTheme } from 'next-themes'
 import { Card, CardHeader, CardContent } from 'component/ui'
+import { cn, KEY } from 'lib'
 import ProductPrice from './product-price'
 import ProductRating from './product-rating'
 
@@ -11,6 +15,9 @@ interface ProductCardProps {
 }
 
 const ProductCard: FC<ProductCardProps> = ({ product }) => {
+  const { theme, systemTheme } = useTheme()
+  const _HL =  theme === KEY.LIGHT ? 'text-black' : theme === KEY.DARK ?  ' text-tape' : systemTheme === KEY.DARK ? 'text-tape' : 'text-black'
+
   return (
     <Card className={"w-full max-w-sm sm:max-w-screen-sm align-middle m-auto border-2 rounded-md cursor-pointer -rotate-1 hover:rotate-0 transform transition-all duration-300 shadow"}>
       <CardHeader className="p-0 items-center">
@@ -19,7 +26,7 @@ const ProductCard: FC<ProductCardProps> = ({ product }) => {
         </Link>
       </CardHeader>
       <CardContent className="p-4 grid gap-4">
-        <div className="text-xs text-tape">{product.brand}</div>
+        <div className={cn("text-xs", _HL)}>{product.brand}</div>
         <Link href={PATH_DIR.PRODUCT_VIEW(product.slug)}>
           <h2 className="text-sm font-medium">{product.name}</h2>
         </Link>
