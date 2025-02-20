a030ec71952134ed2b36adc2ab502ec50b07ab8c
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-20 23:30:45 +1300

    [refax]: product card fix

--- component/module/product/product-card.tsx
+++ component/module/product/product-card.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { useTheme } from 'next-themes'
 import { Card, CardHeader, CardContent } from 'component/ui'
-import { cn, KEY } from 'lib'
+import { cn } from 'lib'
 import ProductPrice from './product-price'
 import ProductRating from './product-rating'
 
@@ -16,7 +16,7 @@ interface ProductCardProps {
 
 const ProductCard: FC<ProductCardProps> = ({ product }) => {
   const { theme, systemTheme } = useTheme()
-  const _HL =  theme === KEY.LIGHT ? 'text-black' : theme === KEY.DARK ?  ' text-tape' : systemTheme === KEY.DARK ? 'text-tape' : 'text-black'
+  // const _HL =  theme === 'light' ? 'text-black' : theme === 'dark' ?  'text-tape' : systemTheme === 'light' ? 'text-punkpink' : 'text-black'
 
   return (
     <Card className={"w-full max-w-sm sm:max-w-screen-sm align-middle m-auto border-2 rounded-md cursor-pointer -rotate-1 hover:rotate-0 transform transition-all duration-300 shadow"}>
@@ -26,7 +26,7 @@ const ProductCard: FC<ProductCardProps> = ({ product }) => {
         </Link>
       </CardHeader>
       <CardContent className="p-4 grid gap-4">
-        <div className={cn("text-xs", _HL)}>{product.brand}</div>
+        <div className={cn("text-xs", theme === 'light' ? 'text-black' : theme === 'dark' ?  'text-tape' : systemTheme === 'light' ? 'text-black' : 'text-punkpink')}>{product.brand}</div>
         <Link href={PATH_DIR.PRODUCT_VIEW(product.slug)}>
           <h2 className="text-sm font-medium">{product.name}</h2>
         </Link>
