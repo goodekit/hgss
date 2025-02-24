2aae1d3d53d69b40357a7239b81f258475256e98
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-24 23:23:48 +1300

    fixed height for product images

--- component/module/product/product-image.tsx
+++ component/module/product/product-image.tsx
@@ -12,17 +12,17 @@ const ProductImage: FC<ProductImageProps> = ({ images }) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-7 gap-4 h-[500px]">
        <div className="col-span-1">
-        {images.map((image, index) => (
-          <div
-            key={index}
-            onClick={() => setCurrent(index)}
-            className={cn('cursor-pointer hover:opacity-50 ease-in-out transition', index !== 0 && 'mt-2')}>
-            <Image src={image} alt={'product-image'} width={80} height={80} className={cn(current !== index && 'scale-75')} />
-          </div>
-        ))}
-      </div>
+          {images.map((image, index) => (
+            <div
+              key={index}
+              onClick={() => setCurrent(index)}
+              className={cn('cursor-pointer hover:opacity-50 ease-in-out transition', index !== 0 && 'mt-2')}>
+              <Image src={image} alt={'product-image'} width={80} height={80} className={cn(current !== index && 'scale-75')} />
+            </div>
+          ))}
+        </div>
       <div className={'col-span-6'}>
-        <Image src={images[current]} alt={'product-image'} width={1000} height={1000} className="min-h-[300px] object-cover object-center" priority />
+        <Image src={images[current]} alt={'product-image'} width={1000} height={1000} className="max-h-[480px] object-cover object-center" priority />
       </div>
     </div>
   )
