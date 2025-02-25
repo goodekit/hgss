99fb6972163eea214331feb082f40263a7826aec
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-02-25 23:03:28 +1300

    [schema]: updated max length

--- lib/schema/product-schema.ts
+++ lib/schema/product-schema.ts
@@ -6,12 +6,12 @@ export const currency = z
   .refine((num) => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(Number(num))), 'Price must have exactly 2 decimal places')
 
 export const ProductSchema = z.object({
-  name          : z.string().min(3, 'Name must be at least 3 characters').max(255),
-  slug          : z.string().min(5, 'Slug must be at least 5 characters').max(255),
-  category      : z.string().min(3, 'Category must be at least 5 characters').max(255),
-  model         : z.string().min(3, 'Model must be at least 5 characters').max(255),
-  brand         : z.string().min(3, 'Brand must be at least 5 characters').max(255),
-  description   : z.string().min(3, 'Slug must be at least 5 characters').max(255),
+  name          : z.string().min(3, 'Name must be at least 3 characters').max(50),
+  slug          : z.string().min(5, 'Slug must be at least 5 characters').max(50),
+  category      : z.string().min(3, 'Category must be at least 5 characters').max(50),
+  model         : z.string().min(3, 'Model must be at least 5 characters').max(50),
+  brand         : z.string().min(3, 'Brand must be at least 5 characters').max(50),
+  description   : z.string().min(3, 'Slug must be at least 5 characters').max(350),
   specifications: z.array(z.string()),
   stock         : z.coerce.number(),
   images        : z.array(z.string()).min(1, 'Product must be at least 1 image'),
