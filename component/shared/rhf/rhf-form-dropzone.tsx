d4a0eecfd32e861275c4670a077799705ad89e43
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-03-13 00:42:25 +1000

    [action]: implemented delete image

--- component/shared/rhf/rhf-form-dropzone.tsx
+++ component/shared/rhf/rhf-form-dropzone.tsx
@@ -2,12 +2,14 @@
 
 import { z, ZodSchema } from 'zod'
 import { en } from 'public/locale'
+import { IMAGE } from 'hgss-design'
 import { Control, Path, PathValue, UseFormReturn } from 'react-hook-form'
 import Image from 'next/image'
 import { useToast } from 'hook'
+import { X } from 'lucide-react'
+import { deleteProductImage } from 'lib/action'
 import { FormField, FormLabel, FormMessage, FormItem, FormControl } from 'component/ui/form'
 import { Card, CardContent } from 'component/ui/card'
-import { IMAGE } from 'hgss-design'
 import { UploadButton } from 'lib/uploadthing'
 
 type FormKeyLocale = keyof typeof en.form
@@ -22,11 +24,22 @@ interface RHFFormDropzoneProps<TSchema extends ZodSchema> {
 
 const RHFFormDropzone = <TSchema extends ZodSchema>({ control, name, images, formKey, form }: RHFFormDropzoneProps<TSchema>) => {
   const { toast } = useToast()
-
   const handleUploadComplete = (res: { url: string }[]) => {
     const currentImages = form.getValues(name) as string[]
     form.setValue(name, [...currentImages, res[0].url] as PathValue<z.infer<TSchema>, Path<z.infer<TSchema>>>)
   };
+
+  const handleDelete = async (index: number) => {
+    const currentImages = form.getValues(name) as string[]
+    const result = await deleteProductImage(currentImages, index)
+    if (result?.success) {
+      const updatedImages = currentImages.filter((_, _i) => _i != index)
+      form.setValue(name, updatedImages as PathValue<z.infer<TSchema>, Path<z.infer<TSchema>>>)
+      toast({ description: "File deleted successfully" })
+    } else {
+      toast({ variant: 'destructive', description: "Failed to delete file from server" })
+    }
+  }
   return (
     <FormField
         control={control}
@@ -38,7 +51,10 @@ const RHFFormDropzone = <TSchema extends ZodSchema>({ control, name, images, for
                     <CardContent className={'space-y-2 mt-2 items-center min-h-48'}>
                         <div className="flex-start space-x-2">
                             {images.map((image, index) => (
-                                <Image key={index} src={image} height={IMAGE.UPLOAD_THUMBNAIL_H} width={IMAGE.UPLOAD_THUMBNAIL_W} alt={'product-name'} className={'w-20 h-20 object-cover rounded-sm'}  />
+                              <div key={index} className={'relative'}>
+                                <X size={20} color={'red'} className={'absolute top-0 right-0 cursor-pointer'} onClick={() => handleDelete(index)}/>
+                                <Image  src={image} height={IMAGE.UPLOAD_THUMBNAIL_H} width={IMAGE.UPLOAD_THUMBNAIL_W} alt={'product-name'} className={'w-20 h-20 object-cover rounded-sm'}  />
+                              </div>
                             ))}
                             <FormControl>
                               <UploadButton endpoint={'imageUploader'} onClientUploadComplete={handleUploadComplete} onUploadError={(error: Error) => { toast({ variant: 'destructive', description: error.message })}} className={'border-none'} appearance={{  button: 'px-2 bg-transparent', container: 'm-auto' }} />
