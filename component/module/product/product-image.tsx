78c38a7ab88b90656361bc7479401e9343727007
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-03-15 00:18:09 +1000

    [uiux]: layout changes when in mobile and desktop

--- component/module/product/product-image.tsx
+++ component/module/product/product-image.tsx
@@ -12,21 +12,21 @@ const ProductImage: FC<ProductImageProps> = ({ images }) => {
   const [current, setCurrent] = useState(0)
 
   return (
-    <div className="grid grid-cols-2 md:grid-cols-7 gap-4 h-[500px]">
-        <div className="col-span-1">
-          {images.map((image, index) => (
-            <div
-              key={index}
-              onClick={() => setCurrent(index)}
-              className={cn('cursor-pointer hover:opacity-50 ease-in-out transition', index !== 0 && 'mt-2')}>
-              <Image src={image} alt={'product-image'} width={80} height={80} className={cn(current !== index && 'scale-75')} />
-            </div>
-          ))}
-        </div>
-        <div className={'col-span-1 md:col-span-6 flex justify-center items-center'}>
-          <div className={"w-[480px] h-[480px] relative overflow-hidden"}>
-            <Image src={images[current]} alt={'product-image'} fill className="object-cover object-center" priority />
+    <div className="grid grid-cols-1 md:grid-cols-7 spacing-1 gap-4 h-[600px] md:h-[500px]">
+      <div className="flex md:flex-col gap-2 order-2 md:order-1 items-center justify-start">
+        {images.map((image, index) => (
+          <div
+            key={index}
+            onClick={() => setCurrent(index)}
+            className={cn('w-[80px] h-[80px] overflow-hidden cursor-pointer hover:opacity-50 ease-in-out transition')}>
+            <Image src={image} alt={'product-image'} width={80} height={80} className={cn("object-cover object-center" ,current !== index && 'scale-75')} />
           </div>
+        ))}
+      </div>
+      <div className={'flex justify-center items-center order-1 md:order-2 md:col-span-6'}>
+        <div className={"w-[480px] h-[480px] relative overflow-hidden"}>
+          <Image src={images[current]} alt={'product-image'} className={"object-cover object-center"} priority fill/>
+        </div>
       </div>
     </div>
   )
