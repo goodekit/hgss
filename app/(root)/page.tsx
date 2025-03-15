ce75b74f56f4703bc7f9867b1f1cb9c956d5d215
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-03-16 00:44:02 +1000

    [cleanup]

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,11 +9,9 @@ const Homepage = async () => {
     <Fragment>
     <div className="p-4 flex flex-col items-center relative justify-between">
         <div className="w-full max-w-4xl mt-20">
-
           <div className="w-full rounded-lg pb-10 p-5 relative mb-24">
             <Image src={ASSET_DIR.LOGO} priority alt="Homegrown Logo" width={800} height={300} className={"m-auto p-12 pointer-events-none"} />
           </div>
-
           <nav className="special-elite">
             {NAV_CONFIG.map(({ title, href }, index) => (
               <TapeBtn key={index} label={title} href={href} index={index} isLink />
