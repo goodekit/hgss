1116a5a543a8dbcc9f8515a0d6b270cba9fc41e7
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-03-19 17:07:58 +1000

    [code-formatting]: improve code formatting consistency in user menu component

--- component/shared/header/user-menu.tsx
+++ component/shared/header/user-menu.tsx
@@ -30,42 +30,40 @@ const UserMenu = async () => {
             <button className={"rounded-sm flex text-center hover:font-bold text-2xl"}>
               {session?.user?.name ? charAtName(session?.user?.name) : <User2Icon />}
             </button>
-          </div>
         </DropdownMenuTrigger>
-        <DropdownMenuContent className={"w-52 text-sm special-elite"} align="end" forceMount>
+        <DropdownMenuContent className={"w-52 text-sm special-elite"} align={"end"} forceMount>
           <DropdownMenuLabel>
-            <div className="flex flex-col space-y-2">
-              <div className="text-sm font-medium leading-none">{session?.user?.name}</div>
-              <Separator className="my-4" />
+            <div className={"flex flex-col space-y-2"}>
+              <div className={"text-sm font-medium leading-none"}>{session?.user?.name}</div>
+              <Separator className={"my-4"} />
             </div>
           </DropdownMenuLabel>
-          <DropdownMenuItem className="p-2">
+          <DropdownMenuItem className={"p-2"}>
             <ProtectedNavLink href={PATH_DIR.USER.ACCOUNT}>{en.navigation.account.label}</ProtectedNavLink>
           </DropdownMenuItem>
-          <DropdownMenuItem className="p-2">
+          <DropdownMenuItem className={"p-2"}>
             <ProtectedNavLink href={PATH_DIR.USER.ORDER}>{en.order_history.label}</ProtectedNavLink>
           </DropdownMenuItem>
 
-          <Separator className="my-2" />
+          <Separator className={"my-2"} />
           {isAdmin && (
             <Fragment>
-              <DropdownMenuItem className="p-2">
+              <DropdownMenuItem className={"p-2"}>
                 <ProtectedNavLink href={PATH_DIR.ADMIN.OVERVIEW}>{en.admin.label}</ProtectedNavLink>
               </DropdownMenuItem>
-              <Separator className="my-2" />
+              <Separator className={"my-2"}/>
             </Fragment>
           )}
 
-          <DropdownMenuItem className="p-1">
-            <form action={signOutBasic} className="w-full">
-              <Button className="w-full py-4 px-2 h-4 justify-start" variant={'ghost'}>
+          <DropdownMenuItem className={"p-1"}>
+            <form action={signOutBasic} className={"w-full"}>
+              <Button className={"w-full py-4 px-2 h-4 justify-start"} variant={'ghost'}>
                 <LogOut /> {en.sign_out.label}
               </Button>
             </form>
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
-    </div>
   )
 }
 
