d5e22d97d9a957d1e1d2fbd3f0bbf9b861466e61
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-03-19 16:35:15 +1000

    [code-format]: consistency

--- component/shared/header/header.tsx
+++ component/shared/header/header.tsx
@@ -6,18 +6,18 @@ import { ProtectedNavLink } from 'component/shared/protect'
 
 const Header= () => {
   return (
-    <header className="w-full h-[60px]">
-      <div className="wrapper flex-between h-full overflow-hidden">
-        <div className="flex-start">
+    <header className={"w-full h-[60px]"}>
+      <div className={"wrapper flex-between h-full overflow-hidden"}>
+        <div className={"flex-start"}>
           {/* @matt, let me know what to add in the category drwer. */}
           {/* <CategoryDrwr /> */}
           <ProtectedNavLink href={PATH_DIR.ROOT} className={`flex-start ml-4`}>
-            <div className="h-full flex items-center overflow-hidden">
+            <div className={"h-full flex items-center overflow-hidden"}>
               <House size={20}/>
             </div>
           </ProtectedNavLink>
         </div>
-        <div className="hidden">
+        <div className={"hidden"}>
           <Search/>
         </div>
         <Toolbar />
