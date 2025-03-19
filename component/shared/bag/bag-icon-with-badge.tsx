b8f17624ab1a8a25577722b12cdbc8f5f43044f0
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-03-19 17:07:37 +1000

    [code-formatting]: consistency in the code format/styling

--- component/shared/bag/bag-icon-with-badge.tsx
+++ component/shared/bag/bag-icon-with-badge.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react'
 import { ShoppingBagIcon } from 'lucide-react'
 import { PATH_DIR } from 'hgss-dir'
-import { Button, Badge } from 'component/ui'
+import { Badge } from 'component/ui'
 import { ProtectedNavLink } from 'component/shared/protect'
 
 interface BagIconWithBadgeProps {
@@ -11,15 +11,13 @@ interface BagIconWithBadgeProps {
 const BagIconWithBadge: FC<BagIconWithBadgeProps> = ({ itemCount }) => {
   const limit = itemCount > 9 ? '9+' : itemCount
   return (
-    <div className={'relative inline-block'}>
-      <Button asChild variant={'ghost'}>
-        <ProtectedNavLink href={PATH_DIR.BAG} className={'flex items-center'}>
+    <div className={'relative flex items-center inline-block'}>
+        <ProtectedNavLink href={PATH_DIR.BAG}>
             <div className={'relative'}>
               <ShoppingBagIcon size={20} />
               {itemCount > 0 && <Badge variant={'outline'} className={'absolute -top-2 -right-2 px-1 py-0 text-xs rounded-lg bg-red-500 shadow-none text-white'}>{limit}</Badge>}
             </div>
         </ProtectedNavLink>
-      </Button>
     </div>
   )
 }
