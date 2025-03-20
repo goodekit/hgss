fa53fc50322fa48b52d2daf9f1a9c789883285d1
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-03-21 00:17:58 +1000

    [ui-consistency]: reposition Back button for improved layout in contact page

--- app/(root)/contact/page.tsx
+++ app/(root)/contact/page.tsx
@@ -14,11 +14,11 @@ const ContactPage = async () => {
 
   return (
     <Fragment>
+      <div className="mb-12">
+        <BackBtn />
+      </div>
       <div className="grid md:grid-cols-5 md:gap-5">
         <div className="md:col-span-5 space-y-4">
-          <div className="flex-between flex-col md:flex-row my-4">
-            <BackBtn />
-          </div>
           <ContactForm user={user as UserBase} />
         </div>
       </div>
