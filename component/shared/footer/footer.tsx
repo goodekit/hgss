ab885e31c0d2c46ab47ef18543beed9bd93d265d
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-03-21 10:16:22 +1000

    [responsiveness]: enhance footer styling for improved responsiveness on different screen sizes

--- component/shared/footer/footer.tsx
+++ component/shared/footer/footer.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
   const currentYear = new Date().getFullYear()
   return (
     <footer>
-      <div className="p-5 flex-center special-elite">
+      <div className="p-5 flex-center special-elite flex md:text-xl text-xs">
         {currentYear} &copy; {GLOBAL.APP_NAME}. All rights reserved
       </div>
     </footer>
