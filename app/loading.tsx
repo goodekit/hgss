139ce383878d3cd55f7b18dd83c240746bbeec70
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-04-06 23:48:53 +1000

    [cleanup]: remove unused loading text logic from LoadingPage component

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,22 +1,10 @@
 "use client"
 
-// import { useEffect, useState } from 'react'
 import { EllipsisLoader } from 'component/shared/loader'
 
 const LoadingPage = () => {
-  // const [textIndex, setTextIndex] = useState(0)
-  // const loadingTexts = ['Initializing...', 'Loading Assets...', 'Booting System...', 'Checking Memory...', 'Decompressing Files...']
-
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setTextIndex((prev) => (prev + 1) % loadingTexts.length)
-  //   }, 1200)
-  //   return () => clearInterval(interval)
-  // }, [])
-
   return (
     <div className="w-full h-screen flex items-center justify-center text-punk font-mono relative overflow-hidden">
-      {/* bg-[url('/scanlines.png')] for bg */}
       <EllipsisLoader dotSize={'text-4xl'} dotColor={'text-white'}/>
     </div>
   )
