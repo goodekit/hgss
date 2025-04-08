5772968f87871d11fa71e2298838b3b2b4f7805d
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-04-08 23:36:17 +1000

    [cleanup]: remove unnecessary variable assignment in sendPurchaseReceipt function

--- email/index.tsx
+++ email/index.tsx
@@ -6,13 +6,12 @@ const resend = new Resend(GLOBAL.RESEND.RESEND_API_KEY as string)
 
 export const sendPurchaseReceipt = async ({ order }: { order: Order }) => {
    try {
-     const receipt = await resend.emails.send({
+      await resend.emails.send({
        from: `${GLOBAL.APP_NAME} <${GLOBAL.RESEND.SENDER_EMAIL}>`,
        to: order.user.email,
        subject: `Order Confirmation ${order.id}`,
        react: <PurchaseReceiptEmail order={order} />
      })
-     console.log('EMAIL SENT!: ', receipt)
    } catch (error) {
      console.error("error in sending email: ", error)
    }
