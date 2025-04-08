88b5de9736606d07606c1824c2e3425e0f61ff88
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-04-08 11:17:00 +1000

    HGSS-Client[BUG] : No receipt for Stripe Transaction Fixes #9; try catching the error

--- email/index.tsx
+++ email/index.tsx
@@ -5,10 +5,14 @@ import PurchaseReceiptEmail from './purchase-receipt'
 const resend = new Resend(GLOBAL.RESEND.RESEND_API_KEY as string)
 
 export const sendPurchaseReceipt = async ({ order }: { order: Order }) => {
-    await resend.emails.send({
-        from    : `${GLOBAL.APP_NAME} <${GLOBAL.RESEND.SENDER_EMAIL}>`,
-        to      : order.user.email,
-        subject : `Order Confirmation ${order.id}`,
-        react   : <PurchaseReceiptEmail order={order} />
-    })
+   try {
+     await resend.emails.send({
+       from: `${GLOBAL.APP_NAME} <${GLOBAL.RESEND.SENDER_EMAIL}>`,
+       to: order.user.email,
+       subject: `Order Confirmation ${order.id}`,
+       react: <PurchaseReceiptEmail order={order} />
+     })
+   } catch (error) {
+     console.error("error in sending email: ", error)
+   }
 }
