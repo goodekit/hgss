16d929169c671da697fe1bcc0db27185b2914d4f
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-04-13 22:45:38 +1000

    HGSS-Client[Feat] : Update ProductCard to use localized out-of-stock message

--- component/module/product/product-card.tsx
+++ component/module/product/product-card.tsx
@@ -8,6 +8,7 @@ import { useTheme } from 'next-themes'
 import { Card, CardHeader, CardContent } from 'component/ui'
 import { cn } from 'lib'
 import ProductPrice from './product-price'
+import { en } from 'public/locale'
 
 interface ProductCardProps {
   product: Product
@@ -21,7 +22,7 @@ const ProductCard: FC<ProductCardProps> = ({ product }) => {
     <Card className={"w-full max-w-sm sm:max-w-screen-sm special-elite align-middle m-auto border-2 rounded-md cursor-pointer -rotate-1 hover:rotate-0 transform transition-all duration-300 shadow"}>
       <CardHeader className="p-0 items-center">
         <Link href={PATH_DIR.PRODUCT_VIEW(product.slug)}>
-            <Image src={product.images[0]} alt={product.name} height={300} width={300} className={'object-cover object-center'} priority />
+            <Image src={product.images[0]} alt={product.name} fill className={'object-cover object-center h-full w-full'} priority />
         </Link>
       </CardHeader>
       <CardContent className="p-4 grid gap-4">
@@ -30,8 +31,7 @@ const ProductCard: FC<ProductCardProps> = ({ product }) => {
           <h2 className="text-sm font-medium">{product.name}</h2>
         </Link>
         <div className="flex-between gap-4">
-          {/* <ProductRating value={Number(product.rating)}/> */}
-          {product.stock > 0 ? <ProductPrice value={Number(product.price)} /> : <p className="text-destructive">{'Out of Stock'}</p>}
+          {product.stock > 0 ? <ProductPrice value={Number(product.price)} /> : <p className="text-destructive">{en.out_of_stock.label}</p>}
         </div>
       </CardContent>
     </Card>
