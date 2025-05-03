149367284b41264530a1aff3c990b908fbacbf35
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-05-03 22:37:15 +1000

    HGSS-Client : Localization #19 [feat]: Add localization utility for retrieving localized messages

---
+++ lib/util/locale.ts
@@ -0,0 +1,50 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { en, fr, es } from 'public/locale'
+
+type NestedKeys<T> = {
+    [K in Extract<keyof T, string>]: T[K] extends Record<string, never> ? `${K}.${NestedKeys<T[K]>}` : K
+}[Extract<keyof T, string>]
+
+type  LocaleLang = 'en' | 'fr' | 'es'
+type  LocaleKey  = NestedKeys<typeof en>
+const locales    = { en, fr, es }
+
+/**
+ * Retrieves a localized message based on the provided key and optional parameters.
+ *
+ * The function navigates through a nested object structure (assumed to be `en`)
+ * using the dot-separated `key` to find the corresponding message. If the message
+ * is found and is a string, it replaces placeholders in the format `{param}` with
+ * the values provided in the `params` object.
+ *
+ * @param key {LocaleKey} - A dot-separated string representing the path to the desired message
+ *                          in the localization object.
+ * @param params - An optional object containing key-value pairs to replace placeholders
+ *                 in the localized message.
+ * @param locale - Language
+ * @returns The localized message with placeholders replaced, or the original key if
+ *          the message is not found or is not a string.
+ */
+export const getLocale = (key: LocaleKey, params?: Record<string, unknown>, locale: LocaleLang = 'en'): string => {
+  const messages = locales[locale] || locales['en']
+  const message = key.split('.').reduce((o: unknown, i: any) => (o ? (o as any)[i] : null), messages)
+
+  if (!message) {
+    return key
+  }
+
+  if (typeof message !== 'string') {
+    return key
+  }
+
+  let result = message as string
+
+  if (params) {
+    Object.keys(params).forEach((param) => {
+      result = result.replace(new RegExp(`{${param}}`, 'g'), String(params[param]))
+    })
+  }
+
+  return result
+}
+
