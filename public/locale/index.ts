06a4db7276a453a5dd03dfa059890367bf805a0f
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-05-03 23:04:54 +1000

    [cleanup]: Remove commented-out exports in locale index for cleaner code

--- public/locale/index.ts
+++ public/locale/index.ts
@@ -1,7 +1,3 @@
-// export { default as en } from './en/common.json'
-// export { default as fr } from './fr/common.json'
-// export { default as es } from './es/common.json'
-
 export * from './en'
 export * from './es'
 export * from './fr'
