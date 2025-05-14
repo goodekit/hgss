a47a2b609ff8411471490f67b7b04d8db9679aa7
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-05-14 10:47:46 +1000

    [design-tokens]:  Refactor COMPONENT backgroundColor and comment out unused code for theme handling; component tokens in  design/component

--- design/token/component.ts
+++ design/token/component.ts
@@ -1,5 +1,11 @@
 import { systemPalette } from "./palette"
 
+// interface SystemTheme {
+//     theme: { theme: string | undefined, systemTheme: string | undefined }
+// }
+
 export const COMPONENT = {
-    backgroundColor: systemPalette.dark.background.primary,
+  backgroundColor: systemPalette.dark.background.primary,
+//   cardLabel: (theme: SystemTheme) =>
+//     theme.theme === 'light' ? 'text-black' : theme.theme === 'dark' ? 'text-tape' : theme.systemTheme === 'light' ? 'text-punkpink' : 'text-black'
 }
