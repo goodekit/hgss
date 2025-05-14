61d7ed6af80c083bfbf6ea5d5886c849db88a047
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-05-14 10:10:02 +1000

    HGSS-Client : Sticky Header #35
    HGSS-Client : [Refactor-fix] Header #47

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,33 +1,13 @@
 import { ReactNode } from 'react'
-import { ASSET_DIR, PATH_DIR } from 'hgss-dir'
-import Image from 'next/image'
-import Link from 'next/link'
-import { Toolbar } from 'component/shared'
-import { AdminSearch } from 'component/admin'
-import MainNav from './main-nav'
+import { AdminHeader } from 'component/admin'
 
 export default function AdminLayout({ children }: Readonly<{ children: ReactNode }>) {
   return (
-      <header className="w-full h-[40px] border-b">
-        <div className="wrapper flex-center h-full overflow-hidden">
-          <div className="flex-start">
-            <Link href={PATH_DIR.ROOT} className={'w-22'}>
-              <div className="h-full flex items-center overflow-hidden">
-                <Image src={ASSET_DIR.LOGO} width={70} height={70} alt={'logo'} className={'object-cover max-h-full'}/>
-              </div>
-            </Link>
-          </div>
-          <MainNav className={'mx-6 hidden md:block'} />
-          <div className={"ml-auto items-center flex"}>
-            <div className={"mr-5"}>
-              <AdminSearch />
-            </div>
-            <Toolbar moduleType={'admin'} />
-          </div>
-        </div>
-      <div className={"flex-1 space-y-4 p-8 pt-6 special-elite container mx-auto"}>
-        <div className={'module-wrapper'}>{children}</div>
+    <div className="flex h-screen flex-col">
+      <AdminHeader />
+      <div className={'flex-1 space-y-4 p-8 pt-6 special-elite container mx-auto'}>
+        <main className={'module-wrapper'}>{children}</main>
       </div>
-    </header>
+    </div>
   )
 }
--- app/user/layout.tsx
+++ app/user/layout.tsx
@@ -1,29 +1,13 @@
 import { ReactNode } from 'react'
-import { ASSET_DIR, PATH_DIR } from 'hgss-dir'
-import Image from 'next/image'
-import Link from 'next/link'
-import { Toolbar } from 'component/shared'
-import MainNav from './main-nav'
+import { UserHeader } from 'component/shared/header'
 
 export default function UserLayout({ children }: Readonly<{ children: ReactNode }>) {
   return (
-    <header className="w-full h-[40px] border-b">
-        <div className="wrapper flex-center h-full overflow-hidden">
-          <div className="flex-start">
-            <Link href={PATH_DIR.ROOT} className={''}>
-              <div className="h-full flex items-center overflow-hidden">
-                <Image src={ASSET_DIR.LOGO} width={70} height={70} alt={'logo'} className={'object-cover max-h-full'}/>
-              </div>
-            </Link>
-          </div>
-          <MainNav className={'mx-6 hidden md:block'} />
-          <div className="ml-auto items-center flex">
-            <Toolbar moduleType={'user'} />
-          </div>
-        </div>
-        <div className="flex-1 space-y-4 p-8 pt-6 special-elite container mx-auto">
-          <div className={'module-wrapper'}>{children}</div>
-        </div>
-    </header>
+    <div className="flex h-screen flex-col">
+      <UserHeader />
+      <div className="flex-1 space-y-4 p-8 pt-6 special-elite container mx-auto">
+        <div className={'module-wrapper'}>{children}</div>
+      </div>
+    </div>
   )
 }
