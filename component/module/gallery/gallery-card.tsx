ca0d11b1dc2d5305e3884c6e665543ee9259cb38
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-05-19 12:01:17 +1000

    HGSS-Client[Feat] : Gallery Card #43
    Description: refactor gallery card component to use galleryItem and fallbackPhoto props
    
    [refactor]: refactor gallery card component to use galleryItem and fallbackPhoto props

--- component/module/gallery/gallery-card.tsx
+++ component/module/gallery/gallery-card.tsx
@@ -4,37 +4,33 @@ import { FC } from 'react'
 import { PATH_DIR } from 'hgss-dir'
 import Link from 'next/link'
 import Image from 'next/image'
-import { useTheme } from 'next-themes'
 import { Card, CardHeader, CardContent } from 'component/ui'
 import { cn } from 'lib'
 
 interface GalleryCardProps {
-  product: Product
+  galleryItem  : GalleryItem
+  fallbackPhoto: string
 }
 
-const GalleryCard: FC<GalleryCardProps> = ({ product }) => {
-  const { theme, systemTheme } = useTheme()
-
+const GalleryCard: FC<GalleryCardProps> = ({ galleryItem, fallbackPhoto }) => {
   return (
     <Card className={'w-full max-w-sm sm:max-w-screen-sm special-elite align-middle m-auto border-2 rounded-md cursor-pointer -rotate-1 hover:rotate-0 transform transition-all duration-300 shadow'}>
       <CardHeader className="p-0 items-center">
-        <Link href={PATH_DIR.PRODUCT_VIEW(product.slug)}>
+        <Link href={PATH_DIR.PRODUCT_VIEW(galleryItem.id)}>
           <Image
-            src={product.images[0]}
-            alt={product.name}
+            src={galleryItem?.image ? galleryItem?.image : fallbackPhoto}
+            alt={galleryItem.title}
             height={300}
             width={300}
-            className={'object-cover object-center h-full w-full'}
+            className={'object-cover object-center h-[250px] w-[250px]'}
             priority
           />
         </Link>
       </CardHeader>
       <CardContent className="p-4 grid gap-4">
-        <div className={cn('text-xs', theme === 'light' ? 'text-black' : theme === 'dark' ? 'text-tape' : systemTheme === 'light' ? 'text-black' : 'text-punkpink')}>
-          {product.brand}
-        </div>
-        <Link href={PATH_DIR.PRODUCT_VIEW(product.slug)}>
-          <h2 className={"text-sm font-medium"}>{product.name}</h2>
+        <div className={cn('text-brand')}>{galleryItem.title}</div>
+        <Link href={PATH_DIR.PRODUCT_VIEW(galleryItem.id)}>
+          <h2 className={"text-sm font-medium"}>{galleryItem.description}</h2>
         </Link>
       </CardContent>
     </Card>
