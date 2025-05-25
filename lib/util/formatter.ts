06c3b4c5ad9197f6e06501020e0d256938a4002b
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-05-25 23:30:39 +1000

    [helper]: add formatText function for text formatting options

--- lib/util/formatter.ts
+++ lib/util/formatter.ts
@@ -108,3 +108,29 @@ export const formatDateTime = (dateString: Date): {dateTime: string, date: strin
       time    : formattedTime
   }
 }
+
+type FormatTextType = 'capitalize' | 'uppercase' | 'lowercase'
+/**
+ * Formats a given text based on the specified type.
+ *
+ * @param {string} text - The text to be formatted.
+ * @param {FormatTextType} type - The type of formatting to apply.
+ *                        Accepted values are:
+ *                        - 'capitalize': Capitalizes the first letter and converts the rest to lowercase.
+ *                        - 'uppercase': Converts the entire text to uppercase.
+ *                        - 'lowercase': Converts the entire text to lowercase.
+ *                        - Any other value will return the text unchanged.
+ * @returns {string} - The formatted text.
+ */
+export function formatText(text: string , type: FormatTextType) {
+  switch (type) {
+    case 'capitalize':
+        return text.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
+    case 'uppercase':
+        return text.toUpperCase()
+    case 'lowercase':
+        return text.toLowerCase()
+    default:
+        return text
+  }
+}
