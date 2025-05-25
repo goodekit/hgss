b1f151e7540ff0e8979cd65a24c6239a602fec56
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-05-25 23:27:16 +1000

    HGSS-Client : Gallery Page 2 #54
    [actions]
    HGSS-Client[Feat] : Add deleteGalleryImage function to manage gallery image deletions

--- lib/action/gallery.action.ts
+++ lib/action/gallery.action.ts
@@ -254,3 +254,52 @@ export async function deleteGallery(galleryId: string) {
     return SystemLogger.errorResponse(error as AppError, CODE.BAD_REQUEST, TAG)
   }
 }
+
+/**
+ * Deletes a gallery image from the current list of images by its index or by its address.
+ *
+ * @param currentImages - An array of image URLs representing the current images.
+ * @param index - The index of the image to delete in the `currentImages` array.
+ * @returns A promise that resolves to an object indicating the success or failure of the operation.
+ *
+ * The returned object has the following structure:
+ * - `success`: A boolean indicating whether the deletion was successful.
+ * - `error` (optional): An error object or message if the deletion failed.
+ *
+ * The function performs the following steps:
+ * 1. Extracts the file key from the image URL using `getFileKeyFromUrl`.
+ * 2. If the file key is valid, it attempts to delete the file using the `UTApi` service.
+ * 3. Returns the result of the deletion operation.
+ *
+ * If any error occurs during the process, it logs the error and returns a failure response.
+ */
+export async function deleteGalleryImage(args: ImageInput) {
+  const getFileKeyFromUrl = (url: string) => {
+    try {
+      const urlParts = url?.split('/')
+      const keyParts = urlParts.slice(3)
+      return keyParts.join('/')
+    } catch (error) {
+      console.error(en.error.failed_extract_file_key, error)
+      return null
+    }
+  }
+
+  const imageToDelete = 'index' in args ? args?.currentImages[args.index] : args.currentImages
+  const fileKey = getFileKeyFromUrl(imageToDelete)
+
+  if (!fileKey) return { success: false, error: en.error.invalid_file_key }
+
+    try {
+      await s3.send(
+        new DeleteObjectCommand({
+          Bucket: GLOBAL.AWS.S3_BUCKET_NAME,
+          Key   : fileKey
+        })
+      )
+      return { success: true }
+    } catch (error) {
+      console.error(en.error.unable_delete, error)
+      return { success: false, error }
+    }
+}
