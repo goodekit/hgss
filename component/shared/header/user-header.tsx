6ae2a7fc7d519766604ea8bc7b1023b87d719fd8
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-05-26 19:30:44 +1000

    [resolve-warning]: image imbalance dimension modification

--- component/shared/header/user-header.tsx
+++ component/shared/header/user-header.tsx
@@ -6,12 +6,12 @@ import MainNav from 'app/user/main-nav'
 
 const UserHeader = () => {
   return (
-    <header className={"sticky top-0 z-50 w-full h-[40px] bg-background"}>
+    <header className={'sticky top-0 z-50 w-full h-[40px] bg-background'}>
       <div className="wrapper flex-center h-full overflow-hidden">
         <div className="flex-start">
-          <Link href={PATH_DIR.ROOT} className={''}>
-            <div className="h-full flex items-center overflow-hidden">
-              <Image src={ASSET_DIR.LOGO} width={70} height={70} alt={'logo'} className={'object-cover max-h-full'} />
+          <Link href={PATH_DIR.ROOT} className={'h-auto w-22'}>
+            <div className="flex items-center overflow-hidden">
+              <Image src={ASSET_DIR.LOGO} height={70} width={50} alt={'logo'} className={'object-cover'} />
             </div>
           </Link>
         </div>
