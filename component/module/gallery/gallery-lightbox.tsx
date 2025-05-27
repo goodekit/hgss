2ba972ce29906c12bcc79bdc07ad38b49f9bcb1f
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-05-27 17:26:32 +1000

    HGSS-Client : Gallery Admin Delete gallery/gallery-item and Update #68
    enhance gallery lightbox for admin: add delete functionality and adjust layout for admin module

--- component/module/gallery/gallery-lightbox.tsx
+++ component/module/gallery/gallery-lightbox.tsx
@@ -1,22 +1,30 @@
 'use client'
 
 import { Fragment, useState, useEffect } from 'react'
+import { en } from 'public/locale'
 import Image from 'next/image'
 import Lightbox, { Slide, Render } from 'yet-another-react-lightbox'
 import { Captions, Thumbnails } from 'yet-another-react-lightbox/plugins'
-import { ASSET_DIR } from 'hgss-dir'
+import { ASSET_DIR, PATH_DIR } from 'hgss-dir'
+import { FileX2, FolderX, SquarePen, View } from 'lucide-react'
+import { Button } from 'component/ui'
+import { LinkBtn } from 'component/shared/btn'
+import { DeleteDialg } from 'component/shared/dialg'
+import { KEY } from 'lib'
+import { deleteGallery, deleteGalleryItem } from 'lib/action'
 
 import 'yet-another-react-lightbox/plugins/captions.css'
 import 'yet-another-react-lightbox/plugins/thumbnails.css'
 import 'yet-another-react-lightbox/styles.css'
 
 interface GalleryLightboxProps {
-  items: GalleryItem[]
+  items      : GalleryItem[]
+  moduleType?: ModuleType
 }
 
 type ThumbnailPositionType = 'bottom' | 'start' | 'end' | 'top' | undefined
 
-export default function GalleryLightbox({ items }: GalleryLightboxProps) {
+export default function GalleryLightbox({ items, moduleType = 'user' }: GalleryLightboxProps) {
     const [index, setIndex] = useState<number | null>(null)
     const [thumbnailProps, setThumbnailProps] = useState({
         border  : 0,
@@ -57,13 +65,37 @@ export default function GalleryLightbox({ items }: GalleryLightboxProps) {
         description: item.description
     }))
 
+    const sameAlbum              = (itm: GalleryItem) => items.filter((_itm) => _itm.galleryId === itm.galleryId )
+    const multipleItemsInAnAlbum = (itm: GalleryItem) => (sameAlbum(itm).length > 1 ? en.delete_album.description_multiple : en.delete_album.description)
+    const lastItemInAnAlbum      = (itm: GalleryItem) => (sameAlbum(itm).length <= 1 ? en.delete_image.description_last_image : en.delete_image.description)
+
   return (
     <Fragment>
       {items?.map(
         (item, i) =>
-          item?.image !== '' && (
+          item?.image !== '' &&
+          (moduleType === KEY.ADMIN ? (
+            <div key={i} className="relative group md:h-[250px] md:w-[250px] rounded-md shadow-md overflow-hidden">
+              <Image
+                priority
+                key={i}
+                src={item?.image}
+                alt={item.title}
+                loading="eager"
+                width={250}
+                height={250}
+                onClick={() => setIndex(i)}
+                className={'h-[250px] w-full object-cover object-center cursor-zoom-in'}
+              />
+              <div className="absolute inset-0 bg-black bg-opacity-50 flex-col items-center justify-center space-y-2 opacity-0 group-hover:opacity-100 flex transition-opacity">
+                <Button size={'sm'} onClick={() => setIndex(i)}>{<View />}</Button>
+                <LinkBtn variant={'outline'} href={PATH_DIR.ADMIN.GALLERY_VIEW(item.galleryId)} className={'text-button'} size={'sm'}>{<SquarePen/>}</LinkBtn>
+                <DeleteDialg id={item.id} label={<FileX2/>} variant={'destructive'} action={deleteGalleryItem} description={lastItemInAnAlbum(item)} />
+                <DeleteDialg id={item.galleryId} label={<FolderX />} variant={'destructive'} action={deleteGallery} description={multipleItemsInAnAlbum(item)} />
+              </div>
+            </div>
+          ) : (
             <Image
-              key={i}
               priority
               src={item?.image}
               alt={item.title}
@@ -71,9 +103,9 @@ export default function GalleryLightbox({ items }: GalleryLightboxProps) {
               width={250}
               height={250}
               onClick={() => setIndex(i)}
-              className={'h-[250px] w-[250px] object-cover object-center cursor-zoom-in rounded-md shadow-md'}
+              className={'h-[250px] w-full object-cover object-center cursor-zoom-in'}
             />
-          )
+          ))
       )}
       {slides && (
         <Lightbox
