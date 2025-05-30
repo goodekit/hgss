5354ce5c17aefef79f8788c695c2eb82ceb486bb
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-05-30 10:35:40 +1000

    HGSS-Client[Feat] : Integrate lambda for dynamic image compression #76
    🐬 [chore]: add sharp library for image processing in upload route

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -3,6 +3,7 @@ import { en } from 'public/locale'
 import { NextRequest, NextResponse } from 'next/server'
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3'
 import { v4 as uuidv4 } from 'uuid'
+import sharp from 'sharp'
 import { CODE } from 'lib'
 
 const s3 = new S3Client({
