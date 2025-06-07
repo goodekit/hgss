2b3b7dea17e9eebb2b05a9a6d72af23dee9a29cf
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-07 11:45:12 +1000

    [locale]: Update product creation page to use translation utility for labels and add error messages

--- app/admin/product/create/page.tsx
+++ app/admin/product/create/page.tsx
@@ -1,19 +1,18 @@
 import { Fragment } from 'react'
 import { Metadata } from 'next'
-import { en } from 'public/locale'
 import { PATH_DIR } from 'hgss-dir'
 import { ProductForm } from 'component/admin'
 import { FormBackBtn } from 'component/shared/btn'
-import { generateTitle } from 'lib'
+import { generateTitle, transl } from 'lib'
 
-export const metadata: Metadata = { title: generateTitle(en.create_product.label, en.admin.label) }
+export const metadata: Metadata = { title: generateTitle(transl('create_product.label'), transl('admin.label')) }
 
 const AdminCreateProductPage = () => {
   return (
     <Fragment>
       <FormBackBtn href={PATH_DIR.ADMIN.PRODUCT} withLink />
-      <h2 className="h2-bold permanent-marker-regular">{en.create_product.label}</h2>
-      <div className="my-8">
+      <h2 className={"h2-bold permanent-marker-regular"}>{transl('create_product.label')}</h2>
+      <div className={"my-8"}>
         <ProductForm type={'create'} />
       </div>
     </Fragment>
