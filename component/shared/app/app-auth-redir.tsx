9c90a2f2488f023d59cdf4b746770463e338d432
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-15 10:14:07 +1000

    HGSS-Client : Reset Password Layout (View) #90
    [refactor]

---
+++ component/shared/app/app-auth-redir.tsx
@@ -0,0 +1,29 @@
+import { PATH_DIR } from "hgss-dir"
+import Link from "next/link"
+import { transl } from "lib/util"
+import { RedirType } from './app.types'
+
+interface AppAuthRedir {
+  type: RedirType
+}
+
+const AppAuthRedir = ({ type }: AppAuthRedir) => {
+    return (
+      <div className={'text-sm text-center text-muted-foreground'}>
+        {transl(type === 'sign-up' ? 'already_have_account.label' : 'dont_have_account.label')}
+        <Link href={PATH_DIR[type === 'sign-up' ? 'SIGN_IN' : 'SIGN_UP']} target="_self" className={'link font-bold'}>
+          &nbsp;{transl(`${type === 'sign-in' ? 'sign_up' : 'sign_in'}.label`)}
+        </Link>
+        {type === 'sign-in' && (
+          <div>
+            <p className={'text-md'}>{'or'}</p>
+            <Link href={PATH_DIR.PASSWORD_FORGOT} target="_self" className={'link font-bold'}>
+              {transl('form.forgot_password.placeholder')}
+            </Link>
+          </div>
+        )}
+      </div>
+    )
+}
+
+export default AppAuthRedir
