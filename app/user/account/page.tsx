3d298ba96b7115fe531a6d935a0bea709c9c05f9
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-15 11:01:55 +1000

    [locale-refactor]: Update User Account Page to use translation utility and improve JSX formatting

--- app/user/account/page.tsx
+++ app/user/account/page.tsx
@@ -4,20 +4,21 @@ import { en } from 'public/locale'
 import { auth } from 'auth'
 import { getUserById } from 'lib/action'
 import { AccountForm } from 'component/shared/form'
+import { transl } from 'lib/util'
 
 export const metadata: Metadata = { title: 'Account' }
 
 const UserAccountPage = async () => {
   const session = await auth()
-  const userId = session?.user?.id
+  const userId  = session?.user?.id
 
   if (!userId) throw new Error(en.error.user_not_found)
   const user = await getUserById(userId)
 
   return <SessionProvider session={session}>
-    <div className="max-w-md mx-auto space-y-4">
-      <h2 className="h2-bold permanent-marker-regular">
-      {en.navigation.account.label}
+    <div className={"max-w-md mx-auto space-y-4"}>
+      <h2 className={"h2-bold permanent-marker-regular"}>
+      {transl('navigation.account.label')}
       </h2>
       <AccountForm user={user} />
     </div>
