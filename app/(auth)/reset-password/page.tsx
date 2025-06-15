2852f64a602d70020c30e7286d249210ad925bf6
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-15 10:03:18 +1000

    HGSS-Client[Feat] : Reset Password Page #83 HGSS-Client : Reset Password Layout (View) #90
    HGSS-Client[Feat] : Enhance Reset Password Flow with Token Validation and Email Prefill

--- app/(auth)/reset-password/page.tsx
+++ app/(auth)/reset-password/page.tsx
@@ -1,45 +1,58 @@
 import { Metadata } from 'next'
-import { en } from 'hgss-locale'
+import { PATH_DIR, ASSET_DIR } from 'hgss-dir'
 import Link from 'next/link'
 import Image from 'next/image'
 import { auth } from 'auth'
 import { redirect } from 'next/navigation'
-import { Card, CardHeader, CardDescription, CardContent } from 'component/ui'
-import { PATH_DIR, ASSET_DIR } from 'hgss-dir'
-import ResetPasswordForm from './reset-password-form'
+import { getResetPasswordTokenUser } from 'lib/action'
+import { Card, CardHeader, CardContent } from 'component/ui'
+import { Label } from 'component/ui/label'
 import { transl } from 'lib'
+import ResetPasswordForm from './reset-password-form'
 
-export const metadata: Metadata = { title: en.sign_in.label }
+export const metadata: Metadata = { title: transl('sign_in.label') }
 
 interface ResetPasswordPageProps {
-  searchParams: Promise<{ callbackUrl: string }>
+  searchParams: { callbackUrl?: string; token?: string }
 }
 
 const ResetPasswordPage = async ({ searchParams }: ResetPasswordPageProps) => {
-  const { callbackUrl } = await searchParams
-  const session         = await auth()
+  const { token, callbackUrl } = searchParams
+  const session                = await auth()
+
+  let email = ''
+  if (token) {
+    const response    = await getResetPasswordTokenUser({ token })
+    if (response.success) {
+      const data  = response.data as { email: string }
+            email = data.email
+    } else {
+      redirect(PATH_DIR.PASSWORD_FORGOT)
+    }
+  }
+
   if (session) {
     redirect(callbackUrl || PATH_DIR.ROOT)
   }
   return (
-    <div className="grid grid-cols-1 md:grid-cols-7 h-screen w-full special-elite">
-      <div className="relative col-span-4 hidden md:block">
-        <Image src={ASSET_DIR.BG}  alt="hgss-background" fill priority sizes={"(max-width: 768px) 100vw, (max-width: 1200px) 80vw, 100vw"} className={'h-screen'} />
+    <div className={'grid grid-cols-1 md:grid-cols-7 h-screen w-full special-elite'}>
+      <div className={'relative col-span-4 hidden md:block'}>
+        <Image src={ASSET_DIR.BG} alt="hgss-background" fill priority sizes={'(max-width: 768px) 100vw, (max-width: 1200px) 80vw, 100vw'} className={'h-screen'} />
       </div>
       <div className={'col-span-3 flex items-center justify-center min-h-screen'}>
-        <div className="max-w-2xl px-6 w-full">
-          <Card className="shadow-none border-none">
-            <CardHeader className="space-y-4">
+        <div className={'max-w-2xl px-6 w-full'}>
+          <Card className={'shadow-none border-none'}>
+            <CardHeader className={'flex justify-center items-center space-y-4'}>
               <Link href={PATH_DIR.ROOT} className="flex-center">
                 <div className="relative w-[120px] h-[120px]">
                   <Image src={ASSET_DIR.LOGO} alt="logo" fill style={{ objectFit: 'contain' }} />
                 </div>
               </Link>
-              <CardDescription className={"text-center"}>{transl('form.reset_password.placeholder')}</CardDescription>
-              <CardContent className={"space-y-4"}>
-                <ResetPasswordForm />
-              </CardContent>
+              <Label className={'text-3xl'}>{transl('change_password.label')}</Label>
             </CardHeader>
+            <CardContent className={'space-y-4 my-4'}>
+              <ResetPasswordForm token={token || ''} verifiedUserEmail={email} />
+            </CardContent>
           </Card>
         </div>
       </div>
--- app/(auth)/reset-password/reset-password-form.tsx
+++ app/(auth)/reset-password/reset-password-form.tsx
@@ -1,60 +1,74 @@
 "use client"
 
-import { useActionState } from 'react'
-import { useFormStatus } from 'react-dom'
-import { en } from 'public/locale'
+import { useEffect, useTransition } from 'react'
 import { PATH_DIR } from 'hgss-dir'
-import { useSearchParams } from 'next/navigation'
-import { signInBasic } from 'lib/action'
-import { signInDefaultValue } from 'lib/schema'
+import { useRouter } from 'next/navigation'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { resetPasswordWithToken } from 'lib/action'
+import { ResetPasswordSchema } from 'lib/schema'
+import { useToast } from 'hook'
 import { Input } from 'component/ui/input'
 import { Label } from 'component/ui/label'
 import { TapeBtn } from 'component/shared/btn'
 import { EllipsisLoader } from 'component/shared/loader'
-import { KEY, RESPONSE } from 'lib/constant'
-import { transl } from 'lib'
+import { KEY } from 'lib/constant'
+import { delay, transl } from 'lib/util'
+import { RHFPasswordField } from 'component/shared/rhf'
 
-const ResetPasswordForm = () => {
-  const [data, action]                  = useActionState(signInBasic, RESPONSE.DEFAULT)
-  const searchParams                    = useSearchParams()
-  const callbackUrl                     = searchParams.get(KEY.CALLBACK_URL) || PATH_DIR.ROOT
+const ResetPasswordForm = ({ token, verifiedUserEmail }: { token: string, verifiedUserEmail: string }) => {
+  const [isPending, startTransition]    = useTransition()
+  const router                          = useRouter()
+  const { toast }                       = useToast()
+  const form                            = useForm<ResetPassword>({
+    resolver     : zodResolver(ResetPasswordSchema),
+    defaultValues: { token, password: '', confirmPassword: '' }
+  })
+
+  const { handleSubmit, register } = form
+
+  useEffect(() => {
+    async function validateTokenAndPrefillEmail() {
+      if (!token) return
+        form.setValue('email', verifiedUserEmail)
+    }
+    validateTokenAndPrefillEmail()
+  }, [token])
 
   const ResetPasswordButton = () => {
-    const { pending } = useFormStatus()
     return (
-      <div className="mb-5">
-        <TapeBtn className={'texture-bg'} disabled={pending} label={pending ? <EllipsisLoader /> : transl('submit.label')} />
+      <div className={"mb-5"}>
+        <TapeBtn className={'texture-bg text-lg md:text-sm'} disabled={isPending} label={isPending ? <EllipsisLoader /> : transl('change_password.label')} />
       </div>
     )
   }
 
-  /**
-   * verify the email if it is registered, then
-   * if it is
-   *  This should send an smtp
-   * if it is not
-   *  dont send anything
-   *
-   * after sending update the page that it has send a password link in the email provided
-   *
-   * link should redirect to change password page.
-   * minimum 8 password, not so strict
-   *
-   */
+  const onSubmit = async (data: { token: string, password: string, confirmPassword: string }) => {
+    startTransition(async () => {
+      const response = await resetPasswordWithToken(data)
+      if (!response.success) {
+        toast({ variant: 'destructive', description: response.message })
+        return
+      }
+      await delay(500)
+      toast({ description: response.message })
+      router.push(PATH_DIR.SIGN_IN)
+    })
+  }
 
-  const renderDataMessage = data && !data.success && <div className="text-center text-destructive">{data.message}</div>
   return (
-    <form action={action}>
-      <input type="hidden" name={KEY.CALLBACK_URL} value={callbackUrl} />
-      {renderDataMessage}
+    <form method={'POST'} onSubmit={handleSubmit(onSubmit)}>
       <div className={'space-y-6'}>
         <div>
-          <Label htmlFor="email">{en.form.email.label}</Label>
-          <Input id={KEY.EMAIL} name={KEY.EMAIL} type={KEY.EMAIL} autoComplete={KEY.EMAIL} defaultValue={signInDefaultValue.email} required />
+          <Label htmlFor={KEY.EMAIL}>{transl('form.email.label')}</Label>
+          <Input type={KEY.EMAIL} autoComplete={KEY.EMAIL} {...register('email')} required disabled />
         </div>
+        <RHFPasswordField label={transl('form.password.label')} register={register} name={'password'} />
+        <RHFPasswordField label={transl('form.confirm_password.label')} register={register} name={'confirmPassword'} />
         <div>
           <ResetPasswordButton />
         </div>
+        <Input type="hidden" {...register('token')} required />
       </div>
     </form>
   )
