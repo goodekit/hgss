97f8dae5bc72b47ed41004f6607c26b2324b016e
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-15 10:01:53 +1000

    HGSS-Client[Feat] : Reset Password Page #83 HGSS-Client : Reset Password Layout (View) #90

---
+++ app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,51 @@
+import { Metadata } from 'next'
+import { en } from 'hgss-locale'
+import { PATH_DIR, ASSET_DIR } from 'hgss-dir'
+import Link from 'next/link'
+import Image from 'next/image'
+import { redirect } from 'next/navigation'
+import { auth } from 'auth'
+import { Card, CardHeader, CardContent } from 'component/ui'
+import { Label } from 'component/ui/label'
+import { transl } from 'lib/util'
+import ForgotPasswordForm from './forgot-password-form'
+
+export const metadata: Metadata = { title: transl('sign_in.label') }
+
+interface ForgotPasswordPageProps {
+  searchParams: Promise<{ callbackUrl: string }>
+}
+
+const ForgotPasswordPage = async ({ searchParams }: ForgotPasswordPageProps) => {
+  const { callbackUrl } = await searchParams
+  const session         = await auth()
+  if (session) {
+    redirect(callbackUrl || PATH_DIR.ROOT)
+  }
+  return (
+    <div className={"grid grid-cols-1 md:grid-cols-7 h-screen w-full special-elite"}>
+      <div className={"relative col-span-4 hidden md:block"}>
+        <Image src={ASSET_DIR.BG} alt="hgss-background" fill priority sizes={'(max-width: 768px) 100vw, (max-width: 1200px) 80vw, 100vw'} className={'h-screen'} />
+      </div>
+      <div className={'col-span-3 flex items-center justify-center min-h-screen'}>
+        <div className={"max-w-2xl px-6 w-full"}>
+          <Card className={"shadow-none border-none"}>
+            <CardHeader className={'flex justify-center items-center space-y-4'}>
+              <Link href={PATH_DIR.ROOT} className="flex-center">
+                <div className={"relative w-[120px] h-[120px]"}>
+                  <Image src={ASSET_DIR.LOGO} alt="logo" fill style={{ objectFit: 'contain' }} />
+                </div>
+              </Link>
+              <Label className={'text-3xl'}>{transl('reset_password.label')}</Label>
+            </CardHeader>
+            <CardContent className={'space-y-4 my-4'}>
+              <ForgotPasswordForm />
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ForgotPasswordPage
