8e8ffe96a80e6e98407e984f097cb5dcb3941941
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-17 23:36:32 +1000

    HGSS-Client : Perf - Caching #121
    [feature]: Add caching keys and TTLs for user and order data

--- config/cache.config.ts
+++ config/cache.config.ts
@@ -3,7 +3,18 @@ export const CACHE_KEY = {
   productById     : (id: string) => `cache:product:${id}`,
   productBySlug   : (slug: string) => `cache:product:${slug}`,
   featuredProducts: 'cache:products:featured',
-  categories      : 'cache:categories:'
+  categories      : 'cache:categories:',
+  myBag           : (userId: string) => `cache:mybag:${userId}`,
+  galleries       : (page = 1) => `cache:galleries:page=${page}`,
+  galleryById     : (id: string) => `cache:gallery:${id}`,
+  galleryItems    : (page = 1) => `cache:galleryitems:page=${page}`,
+  galleryItem     : (id: string) => `cache:galleryitem:${id}`,
+  orders          : (page = 1) => `cache:orders:page=${page}`,
+  myOrders        : (page = 1) => `cache:myorders:page=${page}`,
+  orderById       : (id: string) => `cache:order:${id}`,
+  orderSummary    : `cache:orders:summary`,
+  users           : (page = 1) => `cache:users:page=${page}`,
+  userById        : (id: string) => `cache: user:${id}`
 }
 
 export const CACHE_TTL = {
@@ -12,5 +23,16 @@ export const CACHE_TTL = {
   productById     : 600,
   productBySlug   : 600,
   featuredProducts: 180,
-  categories      : 300
+  categories      : 300,
+  myBag           : 600,
+  galleries       : 300,
+  galleryById     : 600,
+  galleryItems    : 300,
+  galleryItem     : 600,
+  orders          : 300,
+  myOrders        : 300,
+  orderById       : 500,
+  orderSummary    : 300,
+  users           : 300,
+  userById        : 600
 }
