bc7b9982a4bdee6eb7012c7b62943617d86be56b
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-17 12:00:52 +1000

    HGSS-Client[BUG] : Google Auth not signing-in #120
    [feature]: Enhance GoogleSignInBtn to display personalized toast message on sign-in

--- component/shared/google/google-sign-in-btn.tsx
+++ component/shared/google/google-sign-in-btn.tsx
@@ -1,24 +1,31 @@
 "use client"
 
 import { signIn } from 'next-auth/react'
+import { useToast } from 'hook'
 import { TapeBtn } from 'component/shared/btn'
 import { EllipsisLoader } from 'component/shared/loader'
 import { transl } from 'lib/util'
 
 interface GoogleSignInBtnProps {
-  loading ?: boolean
+  userName?: string
+  loading?: boolean
   disabled?: boolean
-  onClick ?: () => void
+  onClick?: () => void
 }
 
-export const GoogleSignInBtn = ({ loading, disabled, onClick }: GoogleSignInBtnProps) => {
+export const GoogleSignInBtn = ({ userName, loading, disabled, onClick }: GoogleSignInBtnProps) => {
+  const { toast }    = useToast()
+  const handleSignIn = () => {
+    onClick?.();
+    signIn('google', { callbackUrl: '/' }).then(() => { toast({ description: transl('success.sign_in_welcome_back', { name: userName || 'Mate' })})})
+  }
   return (
     <div className={'my-5'}>
       <TapeBtn
         className={'texture-bg'}
         disabled={loading || disabled}
         label={disabled ? <EllipsisLoader /> : transl('sign_in.google')}
-        onClick={() => { onClick?.(); signIn('google', { callbackUrl: '/' })}}
+        onClick={handleSignIn}
       />
     </div>
   )
