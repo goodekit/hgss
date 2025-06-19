6ebf2e39955d2bdb1deec81bbf7a9a57442bb001
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-06-19 22:37:45 +1000

    [refactor]: Import CACHE_KEY from cache.config for cache management

--- app/(root)/bag/page.tsx
+++ app/(root)/bag/page.tsx
@@ -1,9 +1,10 @@
 
 import { Metadata } from 'next'
-import { getMyBag } from "lib/action";
-import BagTable from "./bag-table";
+import { getMyBag } from "lib/action"
+import { transl } from 'lib/util'
+import BagTable from "./bag-table"
 
-export const metadata: Metadata = { title: 'Shopping Bag' }
+export const metadata: Metadata = { title: transl('your_bag.label') }
 
 const BagPage = async () => {
     const bag = await getMyBag()
