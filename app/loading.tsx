c37d888e21b3d22adb50478626a63e9019dd9bf0
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-07-19 23:59:36 +1000

    [formatting]

--- app/loading.tsx
+++ app/loading.tsx
@@ -4,8 +4,8 @@ import { EllipsisLoader } from 'component/shared/loader'
 
 const LoadingPage = () => {
   return (
-    <div className="w-full h-screen flex items-center justify-center text-punk font-mono relative overflow-hidden">
-      <EllipsisLoader dotSize={'text-4xl'} />
+    <div className={'w-full h-screen flex items-center justify-center text-punk font-mono relative overflow-hidden permanent-marker-regular'}>
+      <EllipsisLoader dotSize={'text-4xl'} dotColor={'text-default'} />
     </div>
   )
 }
