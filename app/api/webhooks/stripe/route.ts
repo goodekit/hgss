e3864fbd5a69ba8f8e6f4e777e092ac6630ff514
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-07-19 23:58:24 +1000

    [locale]

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -1,8 +1,8 @@
 import { NextRequest, NextResponse } from "next/server"
 import { GLOBAL } from "hgss"
-import { en } from "public/locale"
 import Stripe from "stripe"
 import { updateOrderToPaid } from "lib/action/order.action"
+import { transl } from "lib"
 
 export async function POST(req: NextRequest) {
     const event = await Stripe.webhooks.constructEvent(await req.text(), req.headers.get('stripe-signature') as string, GLOBAL.STRIPE.STRIPE_WEBHOOK_SECRET as string)
@@ -10,8 +10,8 @@ export async function POST(req: NextRequest) {
         const { object } = event.data
         await updateOrderToPaid({ orderId: object.metadata.orderId, paymentResult: { id: object.id, status: 'COMPLETED', email_address: object.billing_details.email!, pricePaid: (object.amount / 100).toFixed() }})
         return NextResponse.json({
-            message: en.success.update_order_paid
+            message: transl('success.update_order_paid')
         })
     }
-    return NextResponse.json({ message: en.error.update_order_paid })
+    return NextResponse.json({ message: transl('error.update_order_paid') })
 }
