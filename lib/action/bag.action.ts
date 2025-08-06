d7906d503ea65fc43dd554fe25c336fdafdbda66
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-08-07 01:11:30 +1000

    [debug]: removed debuggers

--- lib/action/bag.action.ts
+++ lib/action/bag.action.ts
@@ -79,14 +79,9 @@ export async function addItemToBag(data: BagItem) {
         sessionBagId,
         ...calculatePrices([item])
       })
-      console.log('new bag: ', newBag)
-      console.log('add item to bag??')
-
       await prisma.bag.create({ data: newBag })
       await invalidateCache(CACHE_KEY.myBagId(sessionBagId))
       revalidatePath(PATH_DIR.PRODUCT_VIEW(product.slug))
-
-      console.log('did it get to this point?')
       return RESPONSE.SUCCESS(`${product.name} added to bag`)
     } else {
       const existItem = (bag.items as BagItem[]).find((x) => x.productId === item.productId)
@@ -116,7 +111,6 @@ export async function addItemToBag(data: BagItem) {
       return SystemLogger.response(`${product.name} ${existItem ? 'updated in' : 'added to'} bag`, CODE.OK, TAG)
     }
   } catch (error) {
-    console.log('error: ', error)
     return SystemLogger.errorResponse(error as AppError, CODE.BAD_REQUEST, TAG)
   }
 }
