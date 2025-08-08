95242b8e8dfe84c17711ed66b6ccaf82d762b005
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-08-08 10:00:30 +1000

    [schema]: contact and enquiries schema

--- lib/schema/contact-and-enquiries-schema.ts
+++ lib/schema/contact-and-enquiries-schema.ts
@@ -1,7 +1,9 @@
 import { z } from 'zod'
+import { transl } from 'lib/util'
 
 export const ContactAndEnquiriesSchema = z.object({
-  name   : z.string().min(3, 'Name must be at least 3 characters').max(255),
-  email  : z.string().email().min(5, 'Email must be at least 5 characters'),
-  message: z.string().max(500, 'Message cannot exceed 500 characters')
+  name       : z.string().min(3, 'Name must be at least 3 characters').max(255),
+  email      : z.string().email().min(5, 'Email must be at least 5 characters'),
+  message    : z.string().max(500, 'Message cannot exceed 500 characters'),
+  attachments: z.array(z.string()).max(4, transl(`validation.max_default`, { field: 'Attachments', value: 4 }))
 })
