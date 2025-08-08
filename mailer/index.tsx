c2ca5dc24d1e2c5c6dda411abb523b7c49aadc55
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-08-09 00:00:30 +1000

    [mailer]: added mailer func

--- mailer/index.tsx
+++ mailer/index.tsx
@@ -1,8 +1,9 @@
-import { Resend } from 'resend'
+import { Attachment, Resend } from 'resend'
 import { GLOBAL } from 'hgss'
+import { transl } from 'lib/util'
+import ContactAndEnquiryEmail from './contact-and-enquiry'
 import PurchaseReceiptEmail from './purchase-receipt'
 import ResetPasswordEmail from './send-password-reset'
-import { transl } from 'lib/util'
 
 const resend = new Resend(GLOBAL.RESEND.RESEND_API_KEY as string)
 
@@ -31,4 +32,22 @@ export const sendResetPasswordLink = async ({ resetLink, userEmail }: { resetLin
    } catch (error) {
      console.error(transl('error.send_email'), error)
    }
+}
+
+export const sendContactAndEnquiry = async ({ userEmail, name, message, attachments }: { userEmail: string, name: string, message: string, attachments: Attachment[] }) => {
+  const year = new Date().getFullYear()
+  try {
+      const email = await resend.emails.send({
+        from   : `${GLOBAL.APP_NAME} <${GLOBAL.RESEND.CONTACT_AND_ENQUIRY_SENDER_EMAIL}>`,
+        to     : GLOBAL.RESEND.CONTACT_AND_ENQUIRY_RECEIVER_EMAIL,
+        replyTo: userEmail,
+        subject: transl('smtp.contact_and_enquiry.subject'),
+        react  : <ContactAndEnquiryEmail appName={GLOBAL.APP_NAME} name={name} message={message} year={year} siteName={GLOBAL.APP_NAME} />,
+        attachments
+      })
+      return email
+  } catch (error) {
+    console.error(transl('error.send_email'), error)
+    throw new Error(`Failed to send contact and enquiry: ${error}`)
+  }
 }
