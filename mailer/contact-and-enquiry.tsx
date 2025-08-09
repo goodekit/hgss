ffdb7121a68f1a0e3729b51e098ff33f6d103a58
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-08-09 23:55:08 +1000

    [enhance]: added currentdate

--- mailer/contact-and-enquiry.tsx
+++ mailer/contact-and-enquiry.tsx
@@ -3,34 +3,40 @@ import { Html, Head, Preview, Body, Container, Heading, Text, Section, Img, Tail
 import { transl } from 'lib/util'
 
 type ContactAndEnquiryEmailProps = {
-  name    : string
-  message : string
-  year    : number
-  siteName: string
-  appName : string
+  name       : string
+  message    : string
+  year       : number
+  currentDate: string
+  siteName   : string
+  appName    : string
 }
 
-export default function ContactAndEnquiryEmail({ appName, name, message, year, siteName }: ContactAndEnquiryEmailProps) {
+export default function ContactAndEnquiryEmail({ appName, name, message, currentDate, year, siteName }: ContactAndEnquiryEmailProps) {
   return (
     <Html>
       <Head />
       <Preview>{transl('smtp.contact_and_enquiry.subject', { name })}</Preview>
       <Tailwind>
-        <Body className={'min-w-[700px] h-[700px] bg-transparent text-[#999] m-20 items-center align-middle font-sans'}>
-          <Container className={'max-w-[600px] my-10 mx-auto bg-[#0D0E0E] border border-[#1D2020] rounded-lg p-10 shadow-lg'}>
-            <Heading className={'font-sans text-2xl my-10'}>{appName}&nbsp;{transl('smtp.contact_and_enquiry.title')}</Heading>
-
-            <Text className={'text-md'}>From: {name}</Text>
-            <Text>{message}</Text>
-
-            <Section className={'flex justify-center mt-8'}>
-              <Img src={ASSET_DIR.MONO} width={'100'} alt="HGSS Logo" />
+        <Body className={'min-w-[320px] w-auto bg-transparent items-center align-middle text-white font-sans'}>
+          <Section className={'bg-transparent flex justify-center py-6'}>
+            <Img src={ASSET_DIR.LOGO_PRODUCTION_PNG} width={'120'} alt="HGSS Logo" />
+          </Section>
+          <Container className={'max-w-[600px] mx-auto bg-[#0D0E0E]'}>
+            <Section className={'bg-[#999] h-[2px] w-full'} />
+            <Section className={'px-8 py-10 text-white'}>
+              <Heading className={'font-sans text-2xl my-10'}>
+                {appName}&nbsp;{transl('smtp.contact_and_enquiry.title')}
+              </Heading>
+              <Text className={'font-bold text-md text-base mb-1'}>
+                From: {name} (Customer) &nbsp;|&nbsp; {currentDate}
+              </Text>
+              <Text className={'mb-6 whitespace-pre-line'}>{message}</Text>
             </Section>
           </Container>
 
-          <Text className={'text-center text-xs text-[#999] mt-10'}>
+          <Section className={'text-center text-xs text-[#999] pb-4 mt-10'}>
             &copy; {year} {siteName}. {transl('legal.copyright_notice')}
-          </Text>
+          </Section>
         </Body>
       </Tailwind>
     </Html>
