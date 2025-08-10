dd1ef7f3e25c42c53ab5704cf0aa7e8f8c9ba434
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-08-10 23:43:25 +1000

    [refactor]: propagation of refactoring in the pathname

--- config/nav.ts
+++ config/nav.ts
@@ -1,5 +1,4 @@
-import { en } from "public/locale"
-import { connect } from "lib/util"
+import { connect, transl } from "lib/util"
 
 /**
  * Navigation Config
@@ -10,7 +9,7 @@ const ADMIN   = 'admin'
 const USER    = 'user'
 
 const ACCOUNT = 'account'
-const CONTACT = 'contact'
+const ASK     = 'ask'
 const PRODUCT = 'product'
 const GALLERY = 'gallery'
 const ORDER   = 'order'
@@ -20,21 +19,21 @@ const ORDER   = 'order'
 const OVERVIEW = 'overview'
 
 export const NAV_CONFIG = [
-    {title: en.product.products.label, href: connect(PRODUCT), className: 'texture-8-bg' },
-    {title: en.gallery.label, href: connect(GALLERY), className: 'texture-4-bg'},
-    {title: en.contact_and_custom_enquiries.label, href: connect(CONTACT), className: 'texture-2-bg'},
+    {title: transl('product.products.label'), href: connect(PRODUCT), className: 'texture-8-bg' },
+    {title: transl('gallery.label'), href: connect(GALLERY), className: 'texture-4-bg'},
+    {title: transl('contact_and_custom_enquiries.label'), href: connect(ASK), className: 'texture-2-bg'},
 ]
 
 export const NAV_CONFIG_USER = [
-    { title: en.navigation.account.label, href: connect(USER, ACCOUNT) },
-    { title: en.navigation.order.label, href: connect(USER, ORDER) },
+    { title: transl('navigation.account.label'), href: connect(USER, ACCOUNT) },
+    { title: transl('navigation.order.label'), href: connect(USER, ORDER) },
 ]
 
 export const NAV_CONFIG_ADMIN = [
-    { title: en.overview.label, href: connect(ADMIN, OVERVIEW) },
-    { title: en.product.label, href: connect(ADMIN, PRODUCT) },
-    { title: en.order.label, href: connect(ADMIN, ORDER) },
-    { title: en.user.label, href: connect(ADMIN, USER) },
-    { title: en.gallery.label, href: connect(ADMIN, GALLERY) },
+    { title: transl('overview.label'), href: connect(ADMIN, OVERVIEW) },
+    { title: transl('product.label'), href: connect(ADMIN, PRODUCT) },
+    { title: transl('order.label'), href: connect(ADMIN, ORDER) },
+    { title: transl('user.label'), href: connect(ADMIN, USER) },
+    { title: transl('gallery.label'), href: connect(ADMIN, GALLERY) },
 ]
 
