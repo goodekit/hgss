a3579c0f833833bd752118c1732c5b11e904eec4
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-08-10 23:48:06 +1000

    [smtp]: email fixed layout

--- mailer/contact-and-enquiry.tsx
+++ mailer/contact-and-enquiry.tsx
@@ -17,17 +17,17 @@ export default function ContactAndEnquiryEmail({ appName, name, message, current
       <Head />
       <Preview>{transl('smtp.contact_and_enquiry.subject', { name })}</Preview>
       <Tailwind>
-        <Body className={'min-w-[320px] w-auto bg-transparent items-center align-middle text-white font-sans'}>
-          <Section className={'bg-transparent flex justify-center py-6'}>
-            <Img src={ASSET_DIR.LOGO_PRODUCTION_PNG} width={'120'} alt="HGSS Logo" />
-          </Section>
-          <Container className={'max-w-[600px] mx-auto bg-[#0D0E0E]'}>
+        <Body className={'min-w-[320px] bg-transparent items-center align-middle text-white font-sans'}>
+          <Container className={'max-w-[600px] mx-auto bg-[#0D0E0E] rounded-md'}>
             <Section className={'bg-[#999] h-[2px] w-full'} />
             <Section className={'px-8 py-10 text-white'}>
+              <Section className={'min-w-[320px] bg-transparent flex justify-center px-8'}>
+                <Img src={ASSET_DIR.MONO} width={'120'} alt="HGSS Logo" />
+              </Section>
               <Heading className={'font-sans text-2xl my-10'}>
                 {appName}&nbsp;{transl('smtp.contact_and_enquiry.title')}
               </Heading>
-              <Text className={'font-bold text-md text-base mb-1'}>
+              <Text className={'font-bold text-lg mb-1'}>
                 From: {name} (Customer) &nbsp;|&nbsp; {currentDate}
               </Text>
               <Text className={'mb-6 whitespace-pre-line'}>{message}</Text>
