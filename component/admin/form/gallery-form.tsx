f32a98da85b0e65639ed1a0b6ef961bc26731f2b
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-08-11 19:23:56 +1000

    🦄[refactor]: decoupled gallery item form form

--- component/admin/form/gallery-form.tsx
+++ component/admin/form/gallery-form.tsx
@@ -1,20 +1,20 @@
 'use client'
 
 import { FC, Fragment, useEffect } from 'react'
-import { en } from 'public/locale'
 import { PATH_DIR } from 'hgss-dir'
 import { useRouter } from 'next/navigation'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { SubmitHandler, useForm, useFieldArray } from 'react-hook-form'
-import { useToast, usePreventNavigation, useFormState } from 'hook'
-import { Plus, MoveUpRight, X } from 'lucide-react'
+import { useToast, usePreventNavigation, useFormState, useCleanupUnsubmittedImages } from 'hook'
+import { Plus, MoveUpRight } from 'lucide-react'
 import { GallerySchema, UpdateGallerySchema, galleryDefaultValue, galleryItemDefaultValue } from 'lib/schema'
 import { createGallery, updateGallery, deleteGalleryItem } from 'lib/action/gallery.action'
 import { deleteImage } from 'lib/action/image-action'
-import { Form, Button, Card } from 'component/ui'
+import { Form, Button } from 'component/ui'
 import { LoadingBtn } from 'component/shared/btn'
 import { RHFFormField, RHFFormImageUpload } from 'component/shared/rhf'
 import { formatText, delay, cn, transl } from 'lib/util'
+import GalleryItemForm from './gallery-item-form'
 
 const TAG = 'Gallery'
 const GalleryForm: FC<GalleryForm> = ({ type, gallery, galleryId }) => {
@@ -27,14 +27,14 @@ const GalleryForm: FC<GalleryForm> = ({ type, gallery, galleryId }) => {
    defaultValues: isUpdate && gallery ? gallery              : galleryDefaultValue
  })
 
-  const { control, formState, register, handleSubmit } = form
+  const { control, formState, handleSubmit } = form
   const { errors }                                     = formState
   const cover                                          = form.watch('cover')
-  const image                                          = (index:number) =>  form.watch(`galleryItems.${index}.image`)
   const galleryItems                                   = form.getValues(`galleryItems`)
   const { fields, append, remove }                     = useFieldArray<CreateGallery>({ control, name: 'galleryItems' as never })
 
   usePreventNavigation(isDirty)
+  useCleanupUnsubmittedImages(form, 'cover', '__submitted')
 
   useEffect(() => {
     setDirty(formState.isDirty)
@@ -42,7 +42,7 @@ const GalleryForm: FC<GalleryForm> = ({ type, gallery, galleryId }) => {
 
  const onSubmit: SubmitHandler<UpdateGallery | CreateGallery> = async (data) => {
     if (Object.keys(errors).length > 0) {
-      toast({ variant: 'destructive', description: en.error.form });
+      toast({ variant: 'destructive', description: transl('error.form') });
       return
     }
     try {
@@ -66,9 +66,9 @@ const GalleryForm: FC<GalleryForm> = ({ type, gallery, galleryId }) => {
           router.push(PATH_DIR.ADMIN.GALLERY)
         }
       }
-      // router.push(PATH_DIR.ADMIN.GALLERY)
+      form.setValue('__submitted', true)
     } catch (error) {
-      toast({ variant: 'destructive', description: en.error.gallery_save + error })
+      toast({ variant: 'destructive', description: transl('error.gallery_save') + error })
     }
   }
 
@@ -80,20 +80,19 @@ const GalleryForm: FC<GalleryForm> = ({ type, gallery, galleryId }) => {
         if (itemId) {
           const response = await deleteGalleryItem(itemId)
           if (!response.success) {
-            toast({ variant: 'destructive', description: en.error.failed_delete_file })
+            toast({ variant: 'destructive', description: transl('error.failed_delete_file') })
           } else {
-            toast({ description: en.success.deleted })
+            toast({ description: transl('success.deleted')})
           }
         } else {
           const imageResponse = await deleteImage({ currentImages: image })
           if (!imageResponse.success) {
-            toast({ variant: 'destructive', description: en.error.failed_delete_file })
+            toast({ variant: 'destructive', description: transl('error.failed_delete_file') })
           } else {
-            toast({ description: en.success.deleted })
+            toast({ description: transl('success.deleted') })
           }
         }
       }
-
       remove(index)
     } catch (error: unknown) {
       toast({ variant: 'destructive', description: String(error) })
@@ -104,76 +103,31 @@ const GalleryForm: FC<GalleryForm> = ({ type, gallery, galleryId }) => {
     <Fragment>
       <Form {...form}>
         <form method={'POST'} onSubmit={handleSubmit(onSubmit)} className="space-y-8">
-          <div className="flex flex-col md:flex-row gap-4">
+          <div className={"flex flex-col md:flex-row gap-4"}>
             <RHFFormField control={control} name={'title'} formKey={'title'} withWrapper={false} />
           </div>
-          <div className="upload-field gap-4 h-auto">
-            <RHFFormField control={control} name={'description'} formKey={'description'} type={'textarea'} withWrapper={false} />
-          </div>
-
-          <div className={'w-full flex justify-end'}>
-            <div className={'w-1/3'}>
-              <RHFFormImageUpload control={control} name={'cover'} formKey={'cover'} image={cover} form={form} />
+          <div className={"upload-field flex justify-evenly gap-4 h-auto"}>
+            <div className={'w-2/3'}>
+              <RHFFormField control={control} name={'description'} formKey={'description'} type={'textarea'} withWrapper={false} className={'h-40'} />
             </div>
+            <span className={'w-1/3'}>
+              <RHFFormImageUpload control={control} name={'cover'} formKey={'cover'} image={cover} form={form} />
+            </span>
           </div>
 
-          <div className=" flex flex-col gap-4">
-            {/* TODO: put in the components */}
-            <div className="flex flex-col gap-4">
-              <label>{en.form.gallery_item.label}</label>
+          <div className={"flex flex-col gap-4"}>
+            <div className={"flex flex-col gap-4"}>
+              <label>{transl('form.gallery_item.label')}</label>
               {errors.galleryItems && <p className="text-sm text-destructive">{errors.galleryItems.message as string}</p>}
               {fields.map((field, index) => (
-                <Card key={field.id} className={'relative flex flex-col w-full p-2'}>
-                  {!(isUpdate && galleryItems.filter((item: UpdateGalleryItem) => !!item?.id).length === 1 && (galleryItems[index] as UpdateGalleryItem)?.id) && (
-                    <div className="absolute -top-2 -right-2 md:top-0 md:-right-11">
-                      {image(index) === '' ? (
-                        <Button type={'button'} size={'sm'} variant={'destructive'} className={cn('text-black rounded-l-none')} onClick={() => remove(index)}> <X /> </Button>
-                      ) : (
-                        <Button type={'button'} size={'sm'} variant={'destructive'} className={cn('text-black rounded-l-none')} onClick={() => handleRemoveIndex(image(index), index)}> <X /> </Button>
-                      )}
-                    </div>
-                  )}
-                  <input type="hidden" {...register(`galleryItems.${index}.id`)} />
-                  <div className={'flex flex-col md:flex-row justify-evenly gap-2'}>
-                    <div className={'w-full'}>
-                      <RHFFormField
-                        control={control}
-                        formKey={'title'}
-                        {...register(`galleryItems.${index}.title`)}
-                        withLabel={false}
-                        withBorder={false}
-                        className={'border-b p-2 rounded w-full'}
-                      />
-                      <RHFFormField
-                        control={control}
-                        formKey={'description'}
-                        type={'textarea'}
-                        {...register(`galleryItems.${index}.description`)}
-                        withLabel={false}
-                        withBorder={false}
-                        className={'p-2 rounded w-full'}
-                      />
-                    </div>
-                    <div className={'w-full md:w-1/3'}>
-                      <RHFFormImageUpload<typeof GallerySchema>
-                        control={control}
-                        name={`galleryItems.${index}.image`}
-                        formKey={'image'}
-                        image={form.watch(`galleryItems.${index}.image`)}
-                        form={form}
-                        withLabel={false}
-                        folderName={'gallery'}
-                      />
-                    </div>
-                  </div>
-                </Card>
+                <GalleryItemForm key={field.id} form={form} field={field} index={index} remove={remove} handleRemoveIndex={handleRemoveIndex} isUpdate={isUpdate} galleryItems={galleryItems} />
               ))}
               <Button type={'button'} variant={'ghost'} onClick={() => append({ ...galleryItemDefaultValue })} className={cn('font-bold w-full')}>
                 {'+ '}{transl('form.gallery_item.placeholder')}
               </Button>
             </div>
           </div>
-          <div className="flex justify-end">
+          <div className={"flex justify-end"}>
             <LoadingBtn
               variant={'link'}
               isPending={formState.isSubmitting}
