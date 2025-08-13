66ef2e125872308d3b697136e4a952b83235d053
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-08-13 13:37:06 +1000

    [auth]: remove validation for user

--- app/(root)/ask/page.tsx
+++ app/(root)/ask/page.tsx
@@ -1,17 +1,11 @@
 import { Fragment } from 'react'
-import { PATH_DIR } from 'hgss-dir'
 import { auth } from 'auth'
 import { BackBtn } from 'component/shared'
-import { revalidate } from 'lib/util'
 import ContactForm from './ask-form'
 
 const ContactPage = async () => {
   const session = await auth()
-  const user  = session?.user
-
-  if (!user) {
-    revalidate(PATH_DIR.ASK)
-  }
+  const user    = session && session?.user
 
   return (
     <Fragment>
