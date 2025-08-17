06871d66d1472a9479d3bdb8e76b51610bcde637
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-08-17 13:35:44 +1000

    🦄 [feat]: enhance ShippingAddressForm with userName prop, improved address input, and localization support

--- app/(root)/shipping/shipping-address-form.tsx
+++ app/(root)/shipping/shipping-address-form.tsx
@@ -1,7 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 'use client'
 
-import { FC, Fragment, useTransition } from 'react'
-import { en } from 'public/locale'
+import { FC, Fragment, useEffect, useState, useTransition } from 'react'
 import { PATH_DIR } from 'hgss-dir'
 import { useRouter } from 'next/navigation'
 import { z } from 'zod'
@@ -11,29 +11,69 @@ import { useToast } from 'hook'
 import { updateUserAddress } from 'lib/action'
 import { ShippingAddressSchema, shippingAddressDefaultValue } from 'lib/schema'
 import { Form } from 'component/ui/form'
-import { RHFFormField } from 'component/shared/rhf'
+import { Button } from 'component/ui'
+import { RHFFormField, RHFFormCountrySelect, RHFGoogleAddressAutocomplete } from 'component/shared/rhf'
 import { TapeBtn } from 'component/shared/btn'
 import { EllipsisLoader } from 'component/shared/loader'
+import { transl } from 'lib'
 
-interface ShippingAddressFormProps {
-  address: ShippingAddress
-}
+interface ShippingAddressFormProps { address: ShippingAddress, userName?: string }
 
-const ShippingAddressForm: FC<ShippingAddressFormProps> = ({ address }) => {
-  const [isPending, startTransition] = useTransition()
-  const form                         = useForm<z.infer<typeof ShippingAddressSchema>>({
+const ShippingAddressForm: FC<ShippingAddressFormProps> = ({ address, userName }) => {
+  const [isPending, startTransition]          = useTransition()
+  const [isEdit, setIsEditMode]               = useState(() => !address)
+  const [isAddressActive, setIsAddressActive] = useState(false)
+  const form                                  = useForm<z.infer<typeof ShippingAddressSchema>>({
     resolver     : zodResolver(ShippingAddressSchema),
     defaultValues: address || shippingAddressDefaultValue
   })
-  const router                    = useRouter()
-  const { toast }                 = useToast()
-  const { control, handleSubmit } = form
 
-  const onSubmit: SubmitHandler<z.infer<typeof ShippingAddressSchema>> = async (data) => {
+  const router                                           = useRouter()
+  const { toast }                                        = useToast()
+  const { control, handleSubmit, formState: { errors } } = form
+
+  const selectedCountry = form.watch('country')
+
+  useEffect(() => {
+    if (Object.keys(errors).length > 0 && !address) {
+      setIsEditMode(true)
+    }
+  },[errors, address])
+
+  const handleEditToggle = () => {
+    setIsEditMode(!isEdit)
+  }
+
+  const handleOnBlur = () => {
+    const value = form.getValues('address')
+    if (value && value.length > 0) {
+      setIsAddressActive(true)
+    } else {
+      setIsAddressActive(false)
+    }
+  }
+
+  const handleContinue = () => {
+    router.push(PATH_DIR.PAYMENT)
+  }
+
+  const handleUpdateAddress: SubmitHandler<ShippingAddress> = async (data) => {
     startTransition(async () => {
       const response = await updateUserAddress(data)
       if (!response.success) {
-        toast({variant: 'destructive', description: response.message})
+        toast({ variant: 'destructive', description: response.message })
+        return
+      }
+      form.reset(response.data as ShippingAddress)
+      setIsEditMode(false)
+    })
+  }
+
+  const onSubmit: SubmitHandler<ShippingAddress> = async (data) => {
+    startTransition(async () => {
+      const response = await updateUserAddress(data)
+      if (!response.success) {
+        toast({ variant: 'destructive', description: response.message })
         return
       }
       router.push(PATH_DIR.PAYMENT)
@@ -42,17 +82,29 @@ const ShippingAddressForm: FC<ShippingAddressFormProps> = ({ address }) => {
 
   return (
     <Fragment>
-      <div className="max-w-md mx-auto space-y-4 special-elite">
-        <h1 className="h2-bold mt-4">{en.shipping_address.label}</h1>
-        <p className="text-sm text-muted-foreground">{en.shipping_address.description}</p>
+      <div className={'max-w-md mx-auto special-elite space-y-5'}>
+        <h1 className={'h2-bold mt-4'}>{transl('shipping_address.label')}</h1>
+        <div className={'flex justify-between items-center'}>
+          <p className={'text-sm text-muted-foreground'}>{isEdit ? transl('shipping_address.description') : transl('form.ship_to.label')}</p>
+
+        </div>
         <Form {...form}>
-          <form method={'post'} onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-            <RHFFormField control={control} name={'fullName'} formKey={'full_name'} />
-            <RHFFormField control={control} name={'streetAddress'} formKey={'street'} />
-            <RHFFormField control={control} name={'city'} formKey={'city'} />
-            <RHFFormField control={control} name={'postalCode'} formKey={'postal_code'} />
-            <RHFFormField control={control} name={'country'} formKey={'country'} />
-            <TapeBtn label={isPending ? <EllipsisLoader /> : en.continue.label} className={'w-full texture-4-bg'} />
+          <form method={'post'} onSubmit={handleSubmit(isEdit ? onSubmit : handleContinue)} className={'space-y-5'}>
+            {isEdit && (
+              <Fragment>
+                <RHFFormField control={control} name={'fullName'} formKey={'full_name'} />
+                <RHFFormCountrySelect control={control} name={'country'} formKey={'country'} />
+                <RHFGoogleAddressAutocomplete control={control} name={'address'} label={'Search your address'} country={selectedCountry} fullName={userName} onBlur={handleOnBlur} onChange={() => setIsAddressActive(true)} />
+              </Fragment>
+            )}
+            {(isEdit && isAddressActive) && <Button type={'button'} variant={'ghost'} onClick={handleSubmit(handleUpdateAddress)} className={'rounded-lg transition ease-in-out'}>{'Save this address'}</Button>}
+            {!isEdit &&(
+              <Fragment>
+                <RHFFormField control={control} name={'address'} formKey={'address'} type={'textarea'} className={'h-auto md:h-50'} disabled />
+                <Button type={'button'} variant={'ghost'} onClick={handleEditToggle} className={'rounded-lg transition ease-in-out'}>{'Use a different address'}</Button>
+              </Fragment>)}
+
+            <TapeBtn label={isPending ? <EllipsisLoader /> : isEdit ? transl('save_and_continue.label') : transl('continue.label')} className={'w-full texture-4-bg'} />
           </form>
         </Form>
       </div>
