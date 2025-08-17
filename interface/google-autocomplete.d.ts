3ac42dafacf32d2afc0f13e88feb554aab863542
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-08-18 00:40:03 +1000

    [formatting]: adjust spacing for improved readability in google-autocomplete.d.ts

--- interface/google-autocomplete.d.ts
+++ interface/google-autocomplete.d.ts
@@ -4,15 +4,15 @@ declare global {
   namespace JSX {
     interface IntrinsicElements {
       'gmp-place-autocomplete': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement> & {
-        placeholder?: string
-        value?: string
+        placeholder            ?: string
+        value                  ?: string
         'included-region-codes'?: string
-        'requested-language'?: string
-        'requested-region'?: string
-        style?: React.CSSProperties
-        onFocus?: () => void
-        onBlur?: () => void
-        onChange?: () => void
+        'requested-language'   ?: string
+        'requested-region'     ?: string
+        style                  ?: React.CSSProperties
+        onFocus                ?: () => void
+        onBlur                 ?: () => void
+        onChange               ?: () => void
       }
     }
   }
