75bf0bf28b3979389f0277c77c09a3df04543a23
bally <108769285+ballyalley-o@users.noreply.github.com>
2025-08-24 12:43:52 +1000

    [fix-build-err/cleanup]: updates props type and cleanup

--- app/admin/user/[id]/page.tsx
+++ app/admin/user/[id]/page.tsx
@@ -1,14 +1,13 @@
 import { FC, Fragment } from 'react'
 import { Metadata } from 'next'
-import { en } from 'public/locale'
 import { PATH_DIR } from 'hgss-dir'
 import { getUserById } from 'lib/action'
 import { notFound } from 'next/navigation'
 import { UserAccountForm } from 'component/admin'
 import { FormBackBtn } from 'component/shared/btn'
-import { generateTitle } from 'lib/util'
+import { generateTitle, transl } from 'lib/util'
 
-export const metadata: Metadata = { title: generateTitle(en.update_product.label, en.admin.label) }
+export const metadata: Metadata = { title: generateTitle(transl('update_product.label'), transl('admin.label') }
 
 interface AdminUserUpdatePageProps {
   params: Promise<{ id: string }>
@@ -20,8 +19,8 @@ const AdminUserUpdatePage: FC<AdminUserUpdatePageProps> = async ({ params }) => 
   return (
     <Fragment>
         <FormBackBtn href={PATH_DIR.ADMIN.USER} withLink />
-        <h1 className="h2-bold">{en.update_user.label}</h1>
-        <UserAccountForm user={user as UpdateUserAccount} />
+        <h1 className="h2-bold">{transl('update_user.label')}</h1>
+        <UserAccountForm user={user} />
     </Fragment>
   )
 }
