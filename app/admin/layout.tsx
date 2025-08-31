1223c99b51c2bf8007692bcdbd2367cb5308d671
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-08-31 23:07:13 +1000

    [fix]: header scroll at certain height fix

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -3,7 +3,7 @@ import { AdminHeader } from 'component/admin'
 
 export default function AdminLayout({ children }: Readonly<{ children: ReactNode }>) {
   return (
-    <div className="flex h-screen flex-col">
+    <div className={"flex min-h-screen flex-col"}>
       <AdminHeader />
       <div className={'flex-1 space-y-4 p-8 pt-6 special-elite container mx-auto'}>
         <main className={'module-wrapper'}>{children}</main>
