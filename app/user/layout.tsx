501a7aa5e044d4337123f3084b36b5c14c3b31e7
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-08-31 23:07:52 +1000

    [fix]: fix header scrolling up

--- app/user/layout.tsx
+++ app/user/layout.tsx
@@ -3,9 +3,9 @@ import { UserHeader } from 'component/shared/header'
 
 export default function UserLayout({ children }: Readonly<{ children: ReactNode }>) {
   return (
-    <div className="flex h-screen flex-col">
+    <div className={"flex min-h-screen flex-col"}>
       <UserHeader />
-      <div className="flex-1 space-y-4 p-8 pt-6 special-elite container mx-auto">
+      <div className={"flex-1 space-y-4 p-8 pt-6 special-elite container mx-auto"}>
         <div className={'module-wrapper'}>{children}</div>
       </div>
     </div>
