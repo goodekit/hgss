ec21314d44ab02a9f438f3e5bf33488c512f34cb
Bally.Lomibao <bally.lomibao@howickltd.com>
2025-09-28 01:55:15 +1000

    [refactor]: replace connect utility with combine for asset and path management

--- config/dir/path.ts
+++ config/dir/path.ts
@@ -1,9 +1,9 @@
-import { connect, connectUrl } from 'lib/util/connect'
+import { combine, combineUrl } from 'lib/util/combine'
 import { GLOBAL } from 'hgss'
 
 const protect = () => {
   const routesArray = GLOBAL.PROTECTED_ROUTES.split(';') || ""
-  return routesArray.map((route: string) => new RegExp(connect(route)))
+  return routesArray.map((route: string) => new RegExp(combine(route)))
 }
 
 export const PROTECTED_ROUTES = protect()
@@ -13,48 +13,48 @@ const _USER = 'user'
 
 export const PATH_DIR = {
   ADMIN           : {
-                      GALLERY       : connect('admin', 'gallery'),
-                      GALLERY_CREATE: connect('admin', 'gallery', 'create'),
-                      GALLERY_VIEW  : (galleryId: string) => connect('admin', 'gallery', galleryId),
-                      ORDER         : connect('admin', 'order'),
-                      OVERVIEW      : connect('admin', 'overview'),
-                      PRODUCT       : connect('admin', 'product'),
-                      PRODUCT_CREATE: connect('admin', 'product', 'create'),
-                      PRODUCT_VIEW  : (productId: string) =>  connect('admin', 'product', productId),
-                      USER          : connect('admin', 'user'),
-                      USER_VIEW     : (userId: string) => connect('connect', 'user', userId)
+                      GALLERY       : combine('admin', 'gallery'),
+                      GALLERY_CREATE: combine('admin', 'gallery', 'create'),
+                      GALLERY_VIEW  : (galleryId: string) => combine('admin', 'gallery', galleryId),
+                      ORDER         : combine('admin', 'order'),
+                      OVERVIEW      : combine('admin', 'overview'),
+                      PRODUCT       : combine('admin', 'product'),
+                      PRODUCT_CREATE: combine('admin', 'product', 'create'),
+                      PRODUCT_VIEW  : (productId: string) =>  combine('admin', 'product', productId),
+                      USER          : combine('admin', 'user'),
+                      USER_VIEW     : (userId: string) => combine('combine', 'user', userId)
                     },
   API             : {
-                      ASK   : connect(_API, 'ask'),
-                      UPLOAD: connect(_API, 'upload')
+                      ASK   : combine(_API, 'ask'),
+                      UPLOAD: combine(_API, 'upload')
                     },
-  ASK             : connect('ask'),
-  BAG             : connect('bag'),
-  CHECKOUT        : connect('order', 'checkout'),
-  CONTACT         : connect('contact'),
-  EMAIL_IMAGE     : (img: string) => connectUrl(GLOBAL.SERVER_URL, img),
-  GOOGLE_CALLBACK : connectUrl(GLOBAL.SERVER_URL, 'api', 'auth', 'callback', 'credentials'),
-  MOCK            : connect('__mock', 'sample-data.ts'),
-  ORDER           : connect('order'),
-  ORDER_VIEW      : (id: string) => connect('order', id),
-  PASSWORD_FORGOT : connect('forgot-password'),
-  PASSWORD_RESET  : (token: string) => connectUrl(GLOBAL.SERVER_URL, `reset-password?token=${token}`),
-  PAYMENT         : connect('payment'),
-  PRODUCT         : connect('product'),
-  PRODUCT_VIEW    : (slug: string) => connect('product', slug),
-  PRODUCT_CALLBACK: (slug: string) => connect('sign-in?callbackUrl=/product', slug),
+  ASK             : combine('ask'),
+  BAG             : combine('bag'),
+  CHECKOUT        : combine('order', 'checkout'),
+  CONTACT         : combine('contact'),
+  EMAIL_IMAGE     : (img: string) => combineUrl(GLOBAL.SERVER_URL, img),
+  GOOGLE_CALLBACK : combineUrl(GLOBAL.SERVER_URL, 'api', 'auth', 'callback', 'credentials'),
+  MOCK            : combine('__mock', 'sample-data.ts'),
+  ORDER           : combine('order'),
+  ORDER_VIEW      : (id: string) => combine('order', id),
+  PASSWORD_FORGOT : combine('forgot-password'),
+  PASSWORD_RESET  : (token: string) => combineUrl(GLOBAL.SERVER_URL, `reset-password?token=${token}`),
+  PAYMENT         : combine('payment'),
+  PRODUCT         : combine('product'),
+  PRODUCT_VIEW    : (slug: string) => combine('product', slug),
+  PRODUCT_CALLBACK: (slug: string) => combine('sign-in?callbackUrl=/product', slug),
   ROOT            : '/',
-  SEARCH          : connect('search'),
-  SEARCH_QUERY    : (params: string) => connect(`search?${params}`),
-  SEARCH_CATEGORY : (category: string) => connect(`search?category=${category}`),
-  SHIPPING        : connect('shipping'),
-  SIGN_IN         : connect('sign-in'),
-  SIGN_UP         : connect('sign-up'),
-  STRIPE_CALLBACK : (id: string) => connectUrl(GLOBAL.SERVER_URL, 'order', id, 'stripe-payment-success'),
-  UPLOAD          : connect('api', 'upload'),
-  UPLOAD_GENERIC  : (folder: string) => connect('api', 'upload', folder),
+  SEARCH          : combine('search'),
+  SEARCH_QUERY    : (params: string) => combine(`search?${params}`),
+  SEARCH_CATEGORY : (category: string) => combine(`search?category=${category}`),
+  SHIPPING        : combine('shipping'),
+  SIGN_IN         : combine('sign-in'),
+  SIGN_UP         : combine('sign-up'),
+  STRIPE_CALLBACK : (id: string) => combineUrl(GLOBAL.SERVER_URL, 'order', id, 'stripe-payment-success'),
+  UPLOAD          : combine('api', 'upload'),
+  UPLOAD_GENERIC  : (folder: string) => combine('api', 'upload', folder),
   USER            : {
-                      ACCOUNT: connect(_USER, 'account'),
-                      ORDER  : connect(_USER, 'order'),
+                      ACCOUNT: combine(_USER, 'account'),
+                      ORDER  : combine(_USER, 'order'),
   }
 }
